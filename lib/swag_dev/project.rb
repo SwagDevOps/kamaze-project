60da2df23f3f37fad21f4427cb9bb65d01fbe779
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-08 18:57:18 +0200

    project (lib) major changes and improvements

--- lib/swag_dev/project.rb
+++ lib/swag_dev/project.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pathname'
+
 unless Kernel.const_defined?('SwagDev')
   module SwagDev
   end
@@ -7,6 +9,36 @@ end
 
 class SwagDev::Project
   require 'swag_dev/project/concern/versionable'
+  require 'swag_dev/project/concern/helper'
+  require 'swag_dev/project/gem'
 
   include Concern::Versionable
+  include Concern::Helper
+
+  # @return [Symbol]
+  attr_reader :name
+
+  # @return [Hash]
+  attr_reader :version_info
+
+  # @return [Pathname]
+  attr_reader :gem
+
+  def initialize
+    @name = ENV.fetch('PROJECT_NAME').to_sym
+    @version_info = ({
+                       version: subject.VERSION.to_s
+                     }.merge(subject.version_info)).freeze
+    @gem = Gem.new(@name)
+  end
+
+  # Main class (subject of project)
+  #
+  # @return [Class]
+  def subject
+    inflector = helper.get(:inflector)
+    name = self.name.to_s.gsub('-', '/')
+
+    inflector.constantize(inflector.classify(name))
+  end
 end
