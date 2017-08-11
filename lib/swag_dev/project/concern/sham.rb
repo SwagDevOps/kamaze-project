f8fd86721814ea853475195bc4480751d7b0520d
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-11 08:35:20 +0200

    concern/sham (lib) major bugfixes and changes

--- lib/swag_dev/project/concern/sham.rb
+++ lib/swag_dev/project/concern/sham.rb
@@ -4,32 +4,51 @@ require 'swag_dev/project/concern'
 require 'swag_dev/project/sham'
 
 require 'active_support/concern'
+require 'sham'
 
-# Provides a standardized way to retrieve shams
+# Provides a standardized way to define and retrieve shams
 #
 # @see SwagDev::Project::Sham
 module SwagDev::Project::Concern::Sham
   extend ActiveSupport::Concern
 
-  # Define a sham
+  # Define/Retrieve a sham
+  #
+  # Define a sham when a block is given,
+  # or retrieve a sham by its name.
+  # Return nil when retrieving and sham is not already defined.
   #
   # @param [Symbol] name
-  # @return [nil]
   # @yieldreturn [Sham::Config]
+  # @return [self|SwagDev::Project::Struct|nil]
   def sham(name, *args)
-    sham_class.sham!(name, *args)
+    name = name.to_sym
+
+    return shammer.sham(name, *args) unless block_given?
+
+    shammable = shammer.config(name).shammable
+    yield(Sham::Config.new(shammable, name))
+
+    self
   end
 
   # Retrieve a sham
   #
+  # @raise [ArgumentError]
   # @param [Symbol] name
   # @return [SwagDev::Project::Struct]
   def sham!(name, *args)
-    sham_class.sham!(name, *args)
+    name = name.to_sym
+
+    shammer.sham!(name, *args)
   end
 
-  # Get
-  def sham_class
+  protected
+
+  # Get ``Sham`` class
+  #
+  # @return [SwagDev::Project::Sham]
+  def shammer
     SwagDev::Project::Sham
   end
 end
