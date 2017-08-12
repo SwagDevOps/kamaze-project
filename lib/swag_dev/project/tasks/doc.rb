0097ab97a5d69e23973fdcd3a3907c1e52896100
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-12 21:24:49 +0200

    tasks/doc (lib) major changes and improvements

--- lib/swag_dev/project/tasks/doc.rb
+++ lib/swag_dev/project/tasks/doc.rb
@@ -2,71 +2,63 @@
 # frozen_string_literal: true
 #
 # see: https://gist.github.com/chetan/1827484
+# TODO allow ``output-dir`` configuration
 
 require 'swag_dev/project'
 require 'swag_dev/project/tasks/gem'
+require 'swag_dev/project/sham/tasks/doc'
 require 'rake/clean'
 
 project = SwagDev.project
+config = project.sham!('tasks/doc')
 
 CLOBBER.include('doc/')
 
-# Config ------------------------------------------------------------
-
-ignored_patterns = [
-  %r{/\.#},
-  /_flymake\.rb$/,
-]
-
 # Tasks -------------------------------------------------------------
 
-if (yardopts_file = Pathname.new(Dir.pwd).join('.yardopts')).file?
-  desc 'Generate documentation (using YARD)'
-  task doc: ['gem:gemspec'] do
-    [:pathname, :yard, :securerandom].each { |req| require req.to_s }
+desc 'Generate documentation (using YARD)'
+task doc: ['gem:gemspec'] do
+  [:pathname, :yard, :securerandom].each { |req| require req.to_s }
 
-    # internal task name
-    tname = 'doc:t_%s' % SecureRandom.hex(8)
+  # internal task name
+  tname = 'doc:t_%s' % SecureRandom.hex(8)
 
-    YARD::Rake::YardocTask.new(tname) do |t|
-      t.options = proc do
-        require 'shellwords'
+  YARD::Rake::YardocTask.new(tname) do |t|
+    t.options = proc do
+      config.yardopts.options + [
+        '--output-dir', './doc',
+        '--title',
+        '%s v%s' % [project.name, project.version_info[:version]]
+      ]
+    end.call + {
+      false => ['--no-stats'],
+      true  => [],
+    }[ENV['RAKE_DOC_WATCH'].to_i.zero?]
 
-        Shellwords.split(yardopts_file.read) + [
-          '--output-dir', './doc',
-          '--title',
-          '%s v%s' % [project.name, project.version_info[:version]]
-        ]
-      end.call + {
-        false => ['--no-stats'],
-        true  => [],
-      }[ENV['RAKE_DOC_WATCH'].to_i.zero?]
-
-      ignored_patterns.each do |regexp|
-        t.options += ['--exclude', regexp.inspect.gsub(%r{^/|/$}, '')]
-      end
-
-      t.after = proc { Rake::Task['doc:after'].invoke }
+    config.ignored_patterns.each do |regexp|
+      t.options += ['--exclude', regexp.inspect.gsub(%r{^/|/$}, '')]
     end
 
-    Rake::Task[tname].invoke
+    t.after = proc { Rake::Task['doc:after'].invoke }
   end
 
-  namespace :doc do
-    task :after do
-      proc do
-        threads = []
-        Dir.glob('doc/**/*.html').each do |f|
-          threads << Thread.new do
-            f = Pathname.new(f)
-            s = f.read.gsub(/^\s*<meta charset="[A-Z]+-{0,1}[A-Z]+">/,
-                            '<meta charset="UTF-8">')
-            f.write(s)
-          end
-        end
+  Rake::Task[tname].invoke
+end
 
-        threads.map(&:join)
+namespace :doc do
+  task :after do
+    proc do
+      threads = []
+      Dir.glob('doc/**/*.html').each do |f|
+        threads << Thread.new do
+          f = Pathname.new(f)
+          s = f.read.gsub(/^\s*<meta charset="[A-Z]+-{0,1}[A-Z]+">/,
+                          '<meta charset="UTF-8">')
+          f.write(s)
+        end
       end
+
+      threads.map(&:join)
     end
   end
 end
