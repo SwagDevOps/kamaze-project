b68707aa0d0ed773611058d70690828441709503
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-13 23:31:00 +0200

    tasks/doc (lib) major changes and improvements:
    * output-dir retrieved through yard/yardoc (.yardopts)
    * task dependencies are "shammable"

--- lib/swag_dev/project/tasks/doc.rb
+++ lib/swag_dev/project/tasks/doc.rb
@@ -1,23 +1,25 @@
 # coding: utf-8
 # frozen_string_literal: true
 #
-# see: https://gist.github.com/chetan/1827484
-# TODO allow ``output-dir`` configuration
+# @see https://gist.github.com/chetan/1827484
 
 require 'swag_dev/project'
-require 'swag_dev/project/tasks/gem'
 require 'swag_dev/project/sham/tasks/doc'
 require 'rake/clean'
 
 project = SwagDev.project
 config = project.sham!('tasks/doc')
+output_dir = project.yardoc.options[:serializer]&.basepath
+config.dependencies.to_h.values.uniq.each { |req| require req }
 
-CLOBBER.include('doc/')
+# clobber -----------------------------------------------------------
+
+CLOBBER.include(output_dir) if output_dir
 
 # Tasks -------------------------------------------------------------
 
 desc 'Generate documentation (using YARD)'
-task doc: ['gem:gemspec'] do
+task doc: config.dependencies.to_h.keys do
   [:pathname, :yard, :securerandom].each { |req| require req.to_s }
 
   # internal task name
@@ -26,7 +28,6 @@ task doc: ['gem:gemspec'] do
   YARD::Rake::YardocTask.new(tname) do |t|
     t.options = proc do
       config.yardopts.options + [
-        '--output-dir', './doc',
         '--title',
         '%s v%s' % [project.name, project.version_info[:version]]
       ]
@@ -49,11 +50,12 @@ namespace :doc do
   task :after do
     proc do
       threads = []
-      Dir.glob('doc/**/*.html').each do |f|
+      Dir.glob("#{output_dir}/**/*.html").each do |f|
         threads << Thread.new do
           f = Pathname.new(f)
-          s = f.read.gsub(/^\s*<meta charset="[A-Z]+-{0,1}[A-Z]+">/,
-                          '<meta charset="UTF-8">')
+          s = f.read
+               .gsub(/^\s*<meta charset="[A-Z]+-{0,1}[A-Z]+">/,
+                     '<meta charset="UTF-8">')
           f.write(s)
         end
       end
