c5f6b60d7552ad4286af91e1d69edbc4003fcfe9
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-15 21:31:54 +0200

    gem (tasks) unclutter

--- lib/swag_dev/project/tasks/gem.rb
+++ lib/swag_dev/project/tasks/gem.rb
@@ -1,16 +1,8 @@
 # frozen_string_literal: true
 
-require 'swag_dev/project'
 [:gemspec, :package, :install, :push].each do |req|
   require "swag_dev/project/tasks/gem/#{req}"
 end
 
-project = SwagDev.project
-
 desc 'Build all the packages'
 task gem: ['gem:gemspec', 'gem:package']
-
-namespace :gem do
-  desc 'Update gemspec'
-  task gemspec: "#{project.name}.gemspec"
-end
