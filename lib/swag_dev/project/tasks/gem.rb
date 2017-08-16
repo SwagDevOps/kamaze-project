036dfbc16a65443f823e0f390fd5b2b1dc1c7fb7
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-16 07:43:12 +0200

    gem (tasks) push removed from default require(s)

--- lib/swag_dev/project/tasks/gem.rb
+++ lib/swag_dev/project/tasks/gem.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-[:gemspec, :package, :install, :push].each do |req|
+[:gemspec, :package, :install].each do |req|
   require "swag_dev/project/tasks/gem/#{req}"
 end
 
