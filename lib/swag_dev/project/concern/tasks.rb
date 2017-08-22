18b91035847ff03949825834788f29f241966bd4
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-22 18:54:24 +0200

    tasks (concern) introduced

---
+++ lib/swag_dev/project/concern/tasks.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'swag_dev/project/concern'
+require 'active_support/concern'
+
+# Concern tasks
+#
+# Stores tasks to enable and provides method to load them
+module SwagDev::Project::Concern::Tasks
+  extend ActiveSupport::Concern
+
+  # Get tasks
+  #
+  # @return [Array<Symbol>]
+  def tasks
+    (@tasks = @tasks || []).clone
+  end
+
+  # Set tasks
+  #
+  # @param [Array] tasks
+  def tasks=(tasks)
+    tasks = tasks.to_a
+    @tasks ||= []
+
+    @tasks.empty? ? @tasks = tasks : @tasks += tasks
+    @tasks.map!(&:to_sym).sort!.uniq!
+
+    tasks
+  end
+
+  protected
+
+  # Load tasks
+  #
+  # @return [self]
+  def tasks_load!
+    self.tasks.each { |req| require "swag_dev/project/tasks/#{req}" }
+
+    self
+  end
+end
