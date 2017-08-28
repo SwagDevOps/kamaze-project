120b29f548e64c18cf2b24f370b1265d0305f721
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-28 08:37:19 +0200

    definition (dsl) ``task_sham_name`` introduced

--- lib/swag_dev/project/dsl/definition.rb
+++ lib/swag_dev/project/dsl/definition.rb
@@ -15,13 +15,29 @@ module SwagDev::Project::Dsl::Definition
   end
 
   # @see [SwagDev::Project::Concern::Sham.sham]
-  def sham(name, *args, &block)
+  def sham(name = nil, *args, &block)
+    name ||= task_sham_name unless block
+
     project.sham(name, *args, &block)
   end
 
   # @see [SwagDev::Project::Concern::Sham.sham!]
   def sham!(name = nil, *args)
+    name ||= task_sham_name
+
     project.sham!(name, *args)
+  end
+
+  # Get sham (default name) depending on caller
+  #
+  # @api private
+  # @return [String]
+  def task_sham_name
+    'tasks/%s' % caller.grep(%r{/tasks/})
+                       .fetch(0)
+                       .split(%r{/tasks/})
+                       .fetch(1).split('.rb:')
+                       .fetch(0)
   end
 
   # @return [SwagDev::Cli]
