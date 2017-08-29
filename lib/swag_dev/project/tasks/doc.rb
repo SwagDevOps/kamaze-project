ad01103ec2ed8223686ad32a1f044d6abffea7de
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-29 19:06:59 +0200

    doc (tasks) major changes, mostly syntactic changes

--- lib/swag_dev/project/tasks/doc.rb
+++ lib/swag_dev/project/tasks/doc.rb
@@ -6,60 +6,66 @@
 require 'swag_dev/project/dsl'
 require 'rake/clean'
 
+if sham!.prerequisites.grep(/^gem(:|$)/)
+  require 'swag_dev/project/tasks/gem'
+end
+
 # clobber -----------------------------------------------------------
 
 CLOBBER.include(sham!.yardoc.output_dir)
 
-# Tasks -------------------------------------------------------------
+# functions ---------------------------------------------------------
 
-desc 'Generate documentation (using YARD)'
-task doc: [] do
-  [:pathname, :yard, :securerandom].each { |req| require req.to_s }
+utf8fix = lambda do |output_dir|
+  return unless output_dir
 
+  threads = []
+  Dir.glob("#{output_dir}/**/*.html").each do |f|
+    threads << Thread.new do
+      f = Pathname.new(f)
+      s = f.read
+           .gsub(/^\s*<meta charset="[A-Z]+-{0,1}[A-Z]+">/,
+                 '<meta charset="UTF-8">')
+      f.write(s)
+    end
+  end
+
+  threads.map(&:join)
+end
+
+doctask = proc do
   # internal task name
   tname = 'doc:t_%s' % SecureRandom.hex(8)
 
   YARD::Rake::YardocTask.new(tname) do |t|
     t.options = proc do
-      if sham!.yardopts.options.include?('--title')
-        sham!.yardopts.options
-      else
-        sham!.yardopts.options + [
-          '--title',
-          '%s v%s' % [project.name, project.version_info[:version]]
-        ]
-      end
-    end.call + {
-      false => ['--no-stats'],
-      true  => [],
-    }[ENV['RAKE_DOC_WATCH'].to_i.zero?]
+      options = sham!.yardopts.options
+
+      {
+        true  => proc { options },
+        false => proc do
+          options + [
+            '--title',
+            '%s v%s' % [project.name, project.version_info[:version]]
+          ]
+        end
+      }.fetch(options.include?('--title')).call
+    end.call
 
     sham!.ignored_patterns.each do |pattern|
       t.options += ['--exclude', pattern]
     end
-
-    t.after = proc { Rake::Task['doc:after'].invoke }
   end
 
-  Rake::Task[tname].invoke
+  Rake::Task[tname]
 end
 
-namespace :doc do
-  task :after do
-    lambda do |output_dir|
-      return unless output_dir
+# tasks -------------------------------------------------------------
 
-      threads = []
-      Dir.glob("#{output_dir}/**/*.html").each do |f|
-        threads << Thread.new do
-          f = Pathname.new(f)
-          s = f.read
-               .gsub(/^\s*<meta charset="[A-Z]+-{0,1}[A-Z]+">/,
-                     '<meta charset="UTF-8">')
-          f.write(s)
-        end
-      end
-      threads.map(&:join)
-    end.call(sham!.yardoc.output_dir)
-  end
+desc 'Generate documentation (using YARD)'
+task doc: sham!.prerequisites do
+  [:pathname, :yard, :securerandom].each { |req| require req.to_s }
+
+  doctask.call.execute
+  utf8fix.call(sham!.yardoc.output_dir)
 end
