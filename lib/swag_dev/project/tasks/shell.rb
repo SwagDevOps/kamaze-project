ed3dfed52ce4968feb972ebfed0cac48d65cd57b
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-30 08:09:42 +0200

    shell (tasks) minor changes

--- lib/swag_dev/project/tasks/shell.rb
+++ lib/swag_dev/project/tasks/shell.rb
@@ -2,21 +2,22 @@
 
 require 'swag_dev/project/dsl'
 
+banner = proc do
+  patch = defined?(RUBY_PATCHLEVEL) ? 'p%s' % RUBY_PATCHLEVEL : nil
+  parts = ['Ruby',
+           '%s%s' % [RUBY_VERSION, patch],
+           '(%s revision %s)' % [RUBY_RELEASE_DATE, RUBY_REVISION],
+           '[%s]' % RUBY_PLATFORM]
+
+  console.stdout.writeln(parts.join(' '), :green)
+end
+
 # More convenient than ``bundle exec pry``
 desc 'Start ruby REPL'
 task :shell do
   require 'pry'
 
-  banner = proc do
-    patch = defined?(RUBY_PATCHLEVEL) ? 'p%s' % RUBY_PATCHLEVEL : nil
-    parts = ['Ruby',
-             '%s%s' % [RUBY_VERSION, patch],
-             '(%s revision %s)' % [RUBY_RELEASE_DATE, RUBY_REVISION],
-             '[%s]' % RUBY_PLATFORM]
-
-    console.stdout.writeln(parts.join(' '), :green)
-  end
-
   banner.call
+
   Pry.start
 end
