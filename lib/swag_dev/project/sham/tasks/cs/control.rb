d2fd54ac75c3b18bc4fbc7b6b1be247857b6f92d
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-08-30 19:16:48 +0200

    tasks/cs/control (sham) parenting

--- lib/swag_dev/project/sham/tasks/cs/control.rb
+++ lib/swag_dev/project/sham/tasks/cs/control.rb
@@ -4,12 +4,14 @@ require 'swag_dev/project/sham'
 require 'swag_dev/project/sham/tasks/cs'
 
 SwagDev::Project::Sham.define('tasks/cs/control') do |c|
+  parent = SwagDev.project.sham!('tasks/cs')
+
   c.attributes do
     {
-      description: 'Run static code analyzer',
-      options:     ['--fail-level', 'E'],
+      description:   'Run static code analyzer',
+      options:       ['--fail-level', 'E'],
       fail_on_error: true,
-      prerequisites: SwagDev.project.sham!('tasks/doc').prerequisites,
+      prerequisites: parent.prerequisites,
     }
   end
 end
