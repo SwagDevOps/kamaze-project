b10978edf7709a55efb247473a2c866cafa0925c
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-09-01 08:42:52 +0200

    cs/correct (tasks) use copy of control, due to symlink bug

--- lib/swag_dev/project/tasks/cs/correct.rb
+++ lib/swag_dev/project/tasks/cs/correct.rb
@@ -1,1 +1,13 @@
-control.rb
+# frozen_string_literal: true
+
+require 'swag_dev/project/dsl'
+require 'swag_dev/project/tasks/cs'
+require 'pathname'
+
+type = Pathname.new(__FILE__).basename('.rb')
+desc sham!.description
+task "cs:#{type}", [:path] => sham!.prerequisites do |t, args|
+  patterns = args[:path] ? [args[:path]] : project.gem.spec.require_paths
+
+  rubocop(patterns, sham: sham!).invoke
+end
