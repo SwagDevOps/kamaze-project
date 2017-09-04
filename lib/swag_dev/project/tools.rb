5875bf6a27f91fb84feb2aa4e37ffb3fe957bcb7
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-09-04 19:08:17 +0200

    tools (project) introduced

---
+++ lib/swag_dev/project/tools.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+# frozen_string_literal: true
+
+require 'swag_dev/project'
+
+# Namespace for tools
+#
+# Some specific tools
+# developped for «technical project management»,
+# like applying license on the whole project files, setup VM easily…
+module SwagDev::Project::Tools
+end
