74b6d5ee513465d3e15bbbd2f26f66f6e998d09e
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-09-07 18:54:33 +0200

    tools/licenser (sham) introduced

---
+++ lib/swag_dev/project/sham/tools/licenser.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'swag_dev/project/sham'
+
+SwagDev::Project::Sham.define('tools/licenser') do |c|
+  c.attributes do
+    {
+      working_dir: SwagDev.project.working_dir,
+      license:     SwagDev.project.version_info[:license_header],
+      files:       SwagDev.project.gem.spec&.files || []
+    }
+  end
+end
