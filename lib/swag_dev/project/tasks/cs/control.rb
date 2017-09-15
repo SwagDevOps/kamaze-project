edaedb078e22d7f52f47b90e525fcc383db9a5ed
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-09-15 08:12:40 +0200

    cs/control (tasks) uncluttered

--- lib/swag_dev/project/tasks/cs/control.rb
+++ lib/swag_dev/project/tasks/cs/control.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
-require 'swag_dev/project/dsl'
 require 'swag_dev/project/tasks/cs'
-require 'pathname'
 
-type = Pathname.new(__FILE__).basename('.rb')
-desc sham!.description
-task "cs:#{type}", [:path] => sham!.prerequisites do |t, args|
-  patterns = args[:path] ? [args[:path]] : project.gem.spec.require_paths
-
-  rubocop(patterns, sham: sham!).invoke
-end
+cs_task_from_file(__FILE__)
