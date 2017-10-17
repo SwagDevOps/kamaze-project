d161b8a9d4af7f4470d7038ec7111fe35913f988
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-10-17 08:13:25 +0200

    gem/gemspec (tasks) use gemspec_writer

--- lib/swag_dev/project/tasks/gem/gemspec.rb
+++ lib/swag_dev/project/tasks/gem/gemspec.rb
@@ -3,34 +3,11 @@
 require 'swag_dev/project/dsl'
 require 'swag_dev/project/tasks/gem'
 
+writer = project.tools.fetch(:gemspec_writer)
+
 desc 'Update gemspec'
-task 'gem:gemspec': FileList.new(["#{project.name}.gemspec"] + sham!.files)
+task 'gem:gemspec': [writer.to_s]
 
-file "#{project.name}.gemspec": FileList.new(sham!.files) do |task|
-  [:ostruct, :pathname, :gemspec_deps_gen, :tenjin]
-    .each { |req| require req.to_s }
-
-  tools = OpenStruct.new(
-    deps_gen: GemspecDepsGen.new,
-    template: Tenjin::Engine.new(cache: false)
-  )
-
-  files = OpenStruct.new(
-    templated: project.path(sham!.template),
-    generated: project.path(task.name.to_s)
-  )
-
-  spec_id = files.templated
-                 .read
-                 .scan(/Gem::Specification\.new\s+do\s+\|([a-z]+)\|/)
-                 .flatten.fetch(0)
-
-  context = project
-    .version_info
-    .merge(name:         project.name,
-           dependencies: tools.deps_gen
-                              .generate_project_dependencies(spec_id).strip)
-
-  content = tools.template.render(files.templated.to_s, context)
-  files.generated.write(content)
+file writer.to_s => sham!.files do
+  writer.write
 end
