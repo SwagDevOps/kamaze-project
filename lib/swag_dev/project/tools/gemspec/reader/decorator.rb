fa2e35b8bf257177f827c43cb59efe8e48de0d05
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-10-18 08:52:51 +0200

    gemspec/reader/decorator (tools) introduced

---
+++ lib/swag_dev/project/tools/gemspec/reader/decorator.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require 'swag_dev/project/tools/gemspec/reader'
+require 'ostruct'
+
+class SwagDev::Project::Tools::Gemspec::Reader
+end
+
+# Decorator for ``Gem::Specification``
+#
+# @note Decorator is not recursive
+class SwagDev::Project::Tools::Gemspec::Reader::Decorator
+  # @param [Gem::Specification] spec
+  def initialize(spec)
+    @spec = spec
+  end
+
+  # Decorate specification to a given type
+  #
+  # @param [Class|symbol] type
+  # @return [Object]
+  #
+  # @raise [ArgumentError]
+  # @see methods
+  def to(type)
+    raise ArgumentError, "invalid type: #{type}" unless methods[type]
+
+    self.__send__(methods.fetch(type))
+  end
+
+  protected
+
+  # @return [Gem::Specification]
+  attr_reader :spec
+
+  # Provides methods mapping
+  #
+  # @return [Hash]
+  def methods
+    {
+      Hash => :decorate_to_h,
+      hash: :decorate_to_h,
+      OpenStruct => :decorate_to_ostruct,
+      ostruct: :decorate_to_ostruct,
+      open_struct: :decorate_to_ostruct,
+    }
+  end
+
+  # Decorate to obtain a ``Hash``
+  #
+  # @return [Hash]
+  def decorate_to_h
+    result = {}
+
+    spec.instance_variables.each do |k|
+      k = k.to_s.gsub(/^@/, '').to_sym
+
+      result[k] = spec.__send__(k) if spec.respond_to?(k)
+    end
+
+    result
+  end
+
+  # Decorate to obtain an ``OpenStruct``
+  #
+  # @return [OpenStruct]
+  def decorate_to_ostruct
+    OpenStruct.new(decorate_to_h)
+  end
+end
