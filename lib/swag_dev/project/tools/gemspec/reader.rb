7aa6f77b45b26f3880ea4ab084062d2a9eb97ba8
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-10-18 09:08:17 +0200

    gemspec/reader (tools) almost done

--- lib/swag_dev/project/tools/gemspec/reader.rb
+++ lib/swag_dev/project/tools/gemspec/reader.rb
@@ -1,24 +1,53 @@
 # frozen_string_literal: true
 
 require 'swag_dev/project/tools/gemspec'
+require 'swag_dev/project/tools/gemspec/reader/decorator'
 require 'pathname'
+require 'rubygems'
 
-# Read gemspec file
+# Read ``gemspec`` file
+#
+# Retrieve ``Gem::Specification`` through ``read`` method.
 #
 # @see SwagDev::Project
 class SwagDev::Project::Tools::Gemspec::Reader
+  # @return [String]
   attr_accessor :gem_name
 
   attr_writer :project
 
+  attr_writer :working_dir
+
   def initialize
+    @working_dir = Dir.pwd
+
     yield self if block_given?
 
     @gem_name ||= project.name
 
-    [:project, :gem_name].each do |m|
+    [:project, :gem_name, :working_dir].each do |m|
       self.singleton_class.class_eval { protected "#{m}=" }
     end
+  end
+
+  # Read gemspec
+  #
+  # @param [nil|Class|Symbol] as
+  # @return [Gem::Specification|Object]
+  def read(as = nil)
+    spec = nil
+    Dir.chdir(working_dir) do
+      spec = Gem::Specification.load(self.spec_file.to_s)
+    end
+
+    as ? Decorator.new(spec).to(as) : spec
+  end
+
+  # Get spec file path (SHOULD be absolute path)
+  #
+  # @return [Pathname]
+  def spec_file
+    working_dir.join("#{project.name}.gemspec")
   end
 
   # Get project
@@ -27,5 +56,9 @@ class SwagDev::Project::Tools::Gemspec::Reader
   # @return [Object|SwagDev::Project]
   def project
     @project || SwagDev.project
+  end
+
+  def working_dir
+    Pathname.new(@working_dir).realpath
   end
 end
