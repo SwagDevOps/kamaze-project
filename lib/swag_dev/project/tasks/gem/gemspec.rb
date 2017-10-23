e93300f60b7698b04bb6104a6b5b65edce17c0d6
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-10-23 08:40:45 +0200

    gem/gemspec (tasks) does not use sham anymore

--- lib/swag_dev/project/tasks/gem/gemspec.rb
+++ lib/swag_dev/project/tasks/gem/gemspec.rb
@@ -3,11 +3,12 @@
 require 'swag_dev/project/dsl'
 require 'swag_dev/project/tasks/gem'
 
+reader = project.tools.fetch(:gemspec_reader)
 writer = project.tools.fetch(:gemspec_writer)
 
 desc 'Update gemspec'
 task 'gem:gemspec': [writer.to_s]
 
-file writer.to_s => sham!.files do
+file writer.to_s => (reader.read&.files).to_a do
   writer.write
 end
