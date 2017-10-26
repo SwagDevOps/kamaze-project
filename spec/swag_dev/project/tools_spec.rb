fe8a87f800c781221c4df827b8fcd95e7660cd05
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-10-26 09:08:26 +0200

    project/tools (spec) update

--- spec/swag_dev/project/tools_spec.rb
+++ spec/swag_dev/project/tools_spec.rb
@@ -3,14 +3,9 @@
 require 'swag_dev/project/tools'
 
 describe SwagDev::Project::Tools do
-  # instance methods
-  {
-    to_h: [0],
-    get: [1],
-    fetch: [1],
-    '[]': [1],
-    'member?' => [1]
-  }.each do |method, counts|
+  build('project/tools')
+    .describe_instance_methods
+    .each do |method, counts|
     counts.each do |n|
       it { expect(subject).to respond_to(method).with(n).arguments }
     end
