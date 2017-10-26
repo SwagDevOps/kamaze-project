471c41e3e74d32ee7ac25fb4cf98be97b7aa2a68
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-10-26 09:07:23 +0200

    project/tools (spec/factories) describe_instance_methods introduced

--- spec/factories/project/tools.rb
+++ spec/factories/project/tools.rb
@@ -3,6 +3,17 @@
 
 FactoryBot.define do
   factory 'project/tools', class: FactoryStruct do
+    # Describe instance methods
+    #
+    # { method_name: arguments_count [Array] }
+    #
+    # @todo use a custom matcher [DRY]
+    describe_instance_methods({ to_h: [0],
+                                get: [1],
+                                fetch: [1],
+                                '[]': [1],
+                                'member?' => [1] })
+
     # keys COULD be in a different from the given class
     #
     # Usable in context '#to_h' (keys)
