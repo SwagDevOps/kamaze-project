a97895f862c087f3a768596effc636e4d87e8d38
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-10-26 08:41:30 +0200

    factories (spec) introduced

---
+++ spec/factories/tools.rb
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :tools, class: FactoryStruct do
+    # keys COULD be in a different from the given class
+    #
+    # Usable in context '#to_h' (keys)
+    keys([:gemspec_writer,
+          :gemspec_reader,
+          :gemspec_builder,
+          :licenser,
+          :process_locker,
+          :packer,
+          :vagrant])
+  end
+end
