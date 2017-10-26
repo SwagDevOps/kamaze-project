92c33f3a9352caf820f733fd007dc56b7f04b486
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-10-26 08:47:51 +0200

    project/tools (spec/factories) <- tools

--- spec/factories/project/tools.rb
+++ spec/factories/project/tools.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :tools, class: FactoryStruct do
+  factory 'project/tools', class: FactoryStruct do
     # keys COULD be in a different from the given class
     #
     # Usable in context '#to_h' (keys)
