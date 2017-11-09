92808768b8d02e8501e81cb19a8887a70bbb82fd
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-11-09 08:19:32 +0100

    packager/filesystem/utils (tools) purge method added + minor refactor + methods are protected

--- lib/swag_dev/project/tools/packager/filesystem/utils.rb
+++ lib/swag_dev/project/tools/packager/filesystem/utils.rb
@@ -1,19 +1,32 @@
 # frozen_string_literal: true
 
 require 'pathname'
+require 'fileutils'
+require 'rake/file_utils'
+
 require_relative '../filesystem'
 
 # Utilities related to files/paths manipulations
-class SwagDev::Project::Tools::Packager::Filesystem::Utils
-  # Extract directories from given paths
+module SwagDev::Project::Tools::Packager::Filesystem::Utils
+  include FileUtils
+
+  protected
+
+  # Purge a directory
   #
-  # @param [Array<String>] entries
-  # @return [Array<Pathname>]
-  def dirs(paths)
-    paths.map { |path| ::Pathname.new(path) }
-         .map(&:dirname)
-         .delete_if { |path| ['.', '..'].include?(path.basename.to_s) }
-         .uniq.sort
+  # @param [Pathname] dir
+  # @return [Pathname]
+  def purge(dir, options = {})
+    options = { verbose: true }.merge(options)
+    dir = ::Pathname.new(dir)
+
+    if dir.exist?
+      ls(dir).each do |entry|
+        rm_rf(dir.join(entry), options)
+      end
+    end
+
+    dir
   end
 
   # List entries
@@ -24,5 +37,16 @@ class SwagDev::Project::Tools::Packager::Filesystem::Utils
     ::Pathname.new(dir).entries
               .map { |path| ::Pathname.new(path) }
               .delete_if { |path| ['.', '..'].include?(path.basename.to_s) }
+  end
+
+  # Extract directories from given paths
+  #
+  # @param [Array<String>] entries
+  # @return [Array<Pathname>]
+  def map_dirs(paths)
+    paths.map { |path| ::Pathname.new(path) }
+         .map(&:dirname)
+         .delete_if { |path| ['.', '..'].include?(path.basename.to_s) }
+         .uniq.sort
   end
 end
