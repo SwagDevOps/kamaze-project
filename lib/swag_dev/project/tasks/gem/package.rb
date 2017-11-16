ff6172cfe8c6e6854bd09b14268d2e4fb92b98da
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-11-16 18:20:09 +0100

    gem/package (tasks) major changes, use :gemspec_builder

--- lib/swag_dev/project/tasks/gem/package.rb
+++ lib/swag_dev/project/tasks/gem/package.rb
@@ -1,31 +1,15 @@
 # frozen_string_literal: true
 
-require 'swag_dev/project'
-require 'swag_dev/project/tasks/gem'
 require 'rake/clean'
+require_relative '../gem'
 
-project      = SwagDev.project
-dependencies = ['gem:gemspec'] + (project.gem.spec&.files).to_a
+project = SwagDev.project
+builder = project.tools.fetch(:gemspec_builder)
 
-CLOBBER.include('pkg')
+# CLOBBER.include('pkg')
 
-task 'gem:package': FileList.new(*dependencies) do
-  require 'rubygems/package_task'
-  require 'securerandom'
+task 'gem:package': (builder.source_files + [builder.buildable]).to_a do
+  builder.build
 
-  # internal namespace name
-  ns = '_%s' % SecureRandom.hex(4)
-  namespace ns do
-    task = Gem::PackageTask.new(project.gem.spec)
-    task.define
-    # Task management
-    begin
-      Rake::Task['%s:package' % ns].invoke
-    rescue Gem::InvalidSpecificationException => e
-      STDERR.puts(e)
-      exit 1
-    end
-
-    Rake::Task['clobber'].reenable
-  end
+  Rake::Task['clobber'].reenable
 end
