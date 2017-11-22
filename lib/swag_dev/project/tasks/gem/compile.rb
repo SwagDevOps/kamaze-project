656e81d30ad6d3091df19191559a9c07e917f9af
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-11-22 18:59:54 +0100

    gem/compile (tasks) rewrite

--- lib/swag_dev/project/tasks/gem/compile.rb
+++ lib/swag_dev/project/tasks/gem/compile.rb
@@ -1,33 +1,20 @@
 # frozen_string_literal: true
 
-require 'swag_dev/project/dsl'
-require 'swag_dev/project/tasks/gem'
+require 'rake/clean'
+require_relative '../gem'
 
-# tasks --------------------------------------------------------------
-
-tools  = project.tools
-packer = tools.fetch(:packer)
-
-# clobber ``build`` directory SHOULD NOT be a good idea
-#
-# CLOBBER.include(packer.build_dir)
-
-packer.build_dirs.each do |k, dir|
-  directory dir => ['gem:package'] do
-    mkdir_p(dir)
-  end
-end
+tools  = SwagDev.project.tools
+packer = tools.fetch(:gemspec_packer)
 
 packer.buildables.each do |buildable|
   CLOBBER.include(buildable)
 
-  desc "Compile #{buildable.basename}"
-  file buildable => packer.build_dirs.values do
-    tools.fetch(:process_locker).lock!('gem-compile') do
-      packer = tools.fetch(:packer)
-      # packer.__send__('compiler=', :true)
-
+  desc "Compile executable: #{buildable.basename}"
+  file buildable => packer.source_files.to_a.map(&:to_s) do
+    tools.fetch(:process_locker).lock!(:gemspec_packer) do
       packer.pack(buildable)
+
+      Rake::Task['clobber'].reenable
     end
   end
 end
