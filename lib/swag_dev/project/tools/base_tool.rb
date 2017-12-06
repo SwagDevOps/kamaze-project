6d04596a5441f5a410f708c53ba7e44632f0d75c
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-06 09:18:31 +0100

    base_tool (tools) introduced

---
+++ lib/swag_dev/project/tools/base_tool.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative '../tools'
+
+# @abstract
+class SwagDev::Project::Tools::BaseTool
+  def initialize
+    yield self if block_given?
+
+    setup
+
+    attrs_mute!
+  end
+
+  # Mutable attributes
+  #
+  # Mutable attributes become ``ro`` after initialization
+  #
+  # @return [Array]
+  def mutable_attributes
+    []
+  end
+
+  protected
+
+  # Execute additionnal setup
+  def setup
+  end
+
+  def attrs_mute!
+    mutable_attributes.each do |m|
+      self.singleton_class.class_eval { protected "#{m}=" }
+    end
+  end
+end
