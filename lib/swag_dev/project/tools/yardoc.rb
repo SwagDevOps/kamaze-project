9edfc3a6c2c0a501fc84da05c62ae5405ec37324
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-07 08:33:56 +0100

    yardoc (tools) introduced

---
+++ lib/swag_dev/project/tools/yardoc.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require_relative '../tools'
+require_relative 'base_tool'
+require 'pathname'
+require 'yard'
+
+class SwagDev::Project::Tools
+  class Yardoc < BaseTool
+  end
+end
+
+# Tool to run ``CLI::Yardoc`` and generate documentation
+#
+# @see https://github.com/lsegal/yard/blob/49d885f29075cfef4cb954bb9247b6fbc8318cac/lib/yard/rake/yardoc_task.rb
+class SwagDev::Project::Tools::Yardoc
+  # @type [Hash]
+  # @return [Hash]
+  attr_accessor :options
+
+  def run
+    core.run
+  end
+
+  # The output directory. (defaults to ``./doc``)
+  #
+  # @return [Pathname]
+  def output_dir
+    path = core.options.serializer.basepath.gsub(%r{^\./+}, '')
+
+    ::Pathname.new(path)
+  end
+
+  # Ignores files matching path match (regexp)
+  #
+  # @return [Array<String>]
+  def excluded
+    core.excluded
+  end
+
+  protected
+
+  def setup
+    @options ||= {}
+  end
+
+  # @return [YARD::CLI::Yardoc]
+  def core
+    yard = YARD::CLI::Yardoc.new
+    yard.parse_arguments([])
+    options.to_h.each { |k, v| yard.options[k.to_sym] = v }
+
+    yard
+  end
+end
