04234d086ea3791311e1cea2cf15a9bebf184ccb
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-19 08:39:31 +0100

    tools/yardoc/watcher (spec) #paths context added, after random initialization

--- spec/swag_dev/project/tools/yardoc/watcher_spec.rb
+++ spec/swag_dev/project/tools/yardoc/watcher_spec.rb
@@ -11,3 +11,19 @@ describe SwagDev::Project::Tools::Yardoc::Watcher do
     end
   end
 end
+
+10.times do
+  describe SwagDev::Project::Tools::Yardoc::Watcher do
+    let(:paths) { build('project/tools/yardoc/watcher').random_paths }
+
+    subject do
+      described_class.new do |watcher|
+        watcher.paths = paths
+      end
+    end
+
+    context '#paths' do
+      it { expect(subject.paths).to eq(paths) }
+    end
+  end
+end
