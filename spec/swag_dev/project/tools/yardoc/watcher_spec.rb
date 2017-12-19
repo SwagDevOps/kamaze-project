7cfdf24a73dd6110c6d45a9365b3b0a826280f61
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-19 19:11:05 +0100

    tools/yardoc/watcher (spec) tests added

--- spec/swag_dev/project/tools/yardoc/watcher_spec.rb
+++ spec/swag_dev/project/tools/yardoc/watcher_spec.rb
@@ -15,15 +15,30 @@ end
 10.times do
   describe SwagDev::Project::Tools::Yardoc::Watcher do
     let(:paths) { build('project/tools/yardoc/watcher').random_paths }
+    let(:patterns) { build('project/tools/yardoc/watcher').random_patterns }
+    let(:options) { build('project/tools/yardoc/watcher').random_options }
 
     subject do
       described_class.new do |watcher|
         watcher.paths = paths
+        watcher.patterns = patterns
+        watcher.options = options
       end
     end
 
     context '#paths' do
       it { expect(subject.paths).to eq(paths) }
+      it { expect(subject.paths).to be_kind_of(Array) }
+    end
+
+    context '#patterns' do
+      it { expect(subject.patterns).to eq(patterns) }
+      it { expect(subject.patterns).to be_kind_of(Array) }
+    end
+
+    context '#options' do
+      it { expect(subject.options).to eq(options) }
+      it { expect(subject.options).to be_kind_of(Hash) }
     end
   end
 end
