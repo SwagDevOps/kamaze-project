faa97d64bb5fdfca0b725bcbb1f1304b21fb2fbe
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-20 19:20:06 +0100

    console/output (tools) decorate bugfix on tty? + documentation

--- lib/swag_dev/project/tools/console/output.rb
+++ lib/swag_dev/project/tools/console/output.rb
@@ -3,6 +3,16 @@
 require_relative '../console'
 require 'pastel'
 
+# Provide a console output
+#
+# Console output can be initialized from a ``IO``
+# (``STDOUT`` and/or ``STDERR``)
+# a ``File``, using ``File.open()``, or a ``StringIO``.
+#
+# As a result, console provides a convenient way to suppress or replace
+# standard outputs (keeping them untouched).
+#
+# Console output uses ``patsel`` to provide colored formatting.
 class SwagDev::Project::Tools::Console::Output
   # @param [IO] to
   def initialize(to = STDOUT)
@@ -34,6 +44,8 @@ class SwagDev::Project::Tools::Console::Output
 
   # @param [String] s
   def decorate(s, *args)
-    Pastel.new(enabled: output.tty?).decorate(s, *args)
+    tty = output.respond_to?(:tty) ? output.tty? : false
+
+    Pastel.new(enabled: tty).decorate(s, *args)
   end
 end
