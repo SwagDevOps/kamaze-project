1029cac5e9103008466454073d615fdfa72e2c40
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-23 16:35:38 +0100

    gem/gemspec (tasks) improvements + bugfixes

--- lib/swag_dev/project/tasks/gem/gemspec.rb
+++ lib/swag_dev/project/tasks/gem/gemspec.rb
@@ -1,14 +1,19 @@
 # frozen_string_literal: true
 
-require 'swag_dev/project/dsl'
-require 'swag_dev/project/tasks/gem'
+require_relative '../gem'
 
-reader = project.tools.fetch(:gemspec_reader)
-writer = project.tools.fetch(:gemspec_writer)
+tools  = SwagDev.project.tools
+reader = tools.fetch(:gemspec_reader)
+writer = tools.fetch(:gemspec_writer)
+# dependency files
+files  = [writer.templated]
+                .concat((reader.read&.files).to_a)
+                .concat(Dir.glob(['gems.rb', 'gems.locked']))
+                .map(&:to_s)
+                .sort
 
 desc 'Update gemspec'
 task 'gem:gemspec': [writer.to_s]
-
-file writer.to_s => (reader.read&.files).to_a do
+file writer.to_s => files do
   writer.write
 end
