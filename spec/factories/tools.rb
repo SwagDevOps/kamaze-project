467dc3811305adf95ebdcde13c0a936230ced7ae
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-23 15:53:03 +0100

    tools (spec/factories) reformat

--- spec/factories/tools.rb
+++ spec/factories/tools.rb
@@ -8,25 +8,23 @@ FactoryBot.define do
     # { method_name: arguments_count [Array] }
     #
     # @todo use a custom matcher [DRY]
-    describe_instance_methods({ to_h: [0],
-                                get: [1],
-                                fetch: [1],
-                                '[]': [1],
-                                'member?' => [1] })
+    describe_instance_methods(to_h: [0],
+                              get: [1],
+                              fetch: [1],
+                              '[]': [1],
+                              'member?' => [1])
 
     # keys COULD be in a different order from the given class
     #
     # Usable in context '#to_h' (keys)
-    keys([
-           :gemspec_builder,
-           :gemspec_packer,
-           :gemspec_reader,
-           :gemspec_writer,
-           :licenser,
-           :process_locker,
-           :vagrant,
-           :yardoc,
-           :yardoc_watcher,
-         ].shuffle)
+    keys([:gemspec_builder,
+          :gemspec_packer,
+          :gemspec_reader,
+          :gemspec_writer,
+          :licenser,
+          :process_locker,
+          :vagrant,
+          :yardoc,
+          :yardoc_watcher].shuffle)
   end
 end
