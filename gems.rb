5060c1f265abf2fbed2bac09c1edf62554bdc75c
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-23 16:16:25 +0100

    cli-ui (gems) pessimistic dependency removed

--- gems.rb
+++ gems.rb
@@ -4,7 +4,7 @@
 
 source 'https://rubygems.org'
 
-# base
+# base ---------------------------------------------------------------
 gem 'rake', '~> 12.3'
 gem 'activesupport', '~> 5.1'
 gem 'version_info', '~> 1.9'
@@ -12,13 +12,13 @@ gem 'sham', '~> 1.2'
 gem 'cliver', '= 0.3.2'
 gem 'gemspec_deps_gen', '= 1.1.2'
 gem 'tenjin', '~> 0.7'
-gem 'cli-ui', '~> 1.0.0'
+gem 'cli-ui', '~> 1.0', '>= 1.0.0'
 gem 'tty-editor', '~> 0.2'
 gem 'tty-screen', '~> 0.6', '>= 0.6.2'
 gem 'dotenv', '~> 2.2'
 gem 'process_lock', '~> 0.1'
 
-# supported tools
+# supported tools ----------------------------------------------------
 gem 'pry', '~> 0.11'
 gem 'yard', '~> 0.9'
 gem 'rubocop', '~> 0.52'
@@ -26,12 +26,13 @@ gem 'rubocop', '~> 0.52'
 
 group :development do
   gem 'sys-proc', '~> 1.0', '>= 1.0.4'
-  # 'listen' is used to "watch" but could be incompatible with some systems
+
+  # 'listen' is used to "watch"
+  # but could be incompatible with some systems
   gem 'listen', '~> 3.1'
 end
 
 group :doc, :development do
-  # Github Flavored Markdown in YARD
   gem 'redcarpet', '~> 3.4'
   gem 'github-markup', '~> 1.6'
 end
