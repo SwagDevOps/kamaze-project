2dbbf9ae4ba988db84cb95981a1d35014c830bd2
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-23 15:50:24 +0100

    tools/console/output (spec/factories) methods added

--- spec/factories/tools/console/output.rb
+++ spec/factories/tools/console/output.rb
@@ -4,8 +4,10 @@
 FactoryBot.define do
   factory 'tools/console/output', class: FactoryStruct do
     # Describe instance methods
-    describe_instance_methods({
-                                writeln: [1, 2],
-                              })
+    describe_instance_methods('tty?': [0],
+                              flush: [0],
+                              print: [0, 1],
+                              puts: [0, 1],
+                              write: [1])
   end
 end
