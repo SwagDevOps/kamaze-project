8152f3dc53669c76e794b33e356a5ed9ae2f5814
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-26 20:05:42 +0100

    gem/install (tasks) gemspec_builder use + rewrite

--- lib/swag_dev/project/tasks/gem/install.rb
+++ lib/swag_dev/project/tasks/gem/install.rb
@@ -4,17 +4,19 @@
 require 'cliver'
 require_relative '../gem'
 
-project = SwagDev.project
-builder = project.tools.fetch(:gemspec_builder)
-
 desc 'Install gem'
 task 'gem:install': ['gem:build'] do
-  sh(*[Cliver.detect(:sudo),
-       Cliver.detect!(:gem),
-       :install,
-       '-u',
-       '--verbose',
-       '--no-document',
-       '--clear-sources',
-       project.gem.package].compact.map(&:to_s))
+  project = SwagDev.project
+  command = [
+    Cliver.detect(:sudo),
+    Cliver.detect!(:gem),
+    :install,
+    '-u',
+    '--verbose',
+    '--no-document',
+    '--clear-sources',
+    project.tools.fetch(:gemspec_builder).buildable
+  ].compact.map(&:to_s)
+
+  sh(*command)
 end
