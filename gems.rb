fa7e318bdfca46cc38b102a4908a0149b07017ec
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-26 14:07:50 +0100

    Gems should be sorted in an alphabetical order within their section

--- gems.rb
+++ gems.rb
@@ -6,24 +6,24 @@ source 'https://rubygems.org'
 
 # base ---------------------------------------------------------------
 
-gem 'rake', '~> 12.3'
 gem 'activesupport', '~> 5.1'
-gem 'version_info', '~> 1.9'
-gem 'sham', '~> 1.2'
-gem 'cliver', '= 0.3.2'
-gem 'gemspec_deps_gen', '= 1.1.2'
-gem 'tenjin', '~> 0.7'
 gem 'cli-ui', '~> 1.0', '>= 1.0.0'
+gem 'cliver', '= 0.3.2'
+gem 'dotenv', '~> 2.2'
+gem 'gemspec_deps_gen', '= 1.1.2'
+gem 'process_lock', '~> 0.1'
+gem 'rake', '~> 12.3'
+gem 'sham', '~> 1.2'
+gem 'tenjin', '~> 0.7'
 gem 'tty-editor', '~> 0.2'
 gem 'tty-screen', '~> 0.6', '>= 0.6.2'
-gem 'dotenv', '~> 2.2'
-gem 'process_lock', '~> 0.1'
+gem 'version_info', '~> 1.9'
 
 # supported tools ----------------------------------------------------
 
 gem 'pry', '~> 0.11'
-gem 'yard', '~> 0.9'
 gem 'rubocop', '~> 0.52'
+gem 'yard', '~> 0.9'
 # 'rspec' is also supported, but we don't force to use it
 
 # development, doc, test ---------------------------------------------
@@ -37,11 +37,11 @@ group :development do
 end
 
 group :doc, :development do
-  gem 'redcarpet', '~> 3.4'
   gem 'github-markup', '~> 1.6'
+  gem 'redcarpet', '~> 3.4'
 end
 
 group :test, :development do
-  gem 'rspec', '~> 3.7'
   gem 'factory_bot', '~> 4.8'
+  gem 'rspec', '~> 3.7'
 end
