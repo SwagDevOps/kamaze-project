737ceec8886d70a3ac5c8fe070a50fd42f5f496f
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-26 20:38:05 +0100

    helper (spec) rspec context

--- spec/helper.rb
+++ spec/helper.rb
@@ -7,3 +7,6 @@
 ].each { |s| require [__dir__, 'helper', s].map(&:to_s).join('/') }
 
 require "#{__dir__}/../lib/swag_dev-project"
+# same context for ``rspec`` and ``rake``
+require 'rake'
+load("#{__dir__}/../Rakefile")
