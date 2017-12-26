20d9ad763b5a9439022fed945ac97f9d6d760af5
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-26 21:20:09 +0100

    doc/watch (tasks) process_locker use

--- lib/swag_dev/project/tasks/doc/watch.rb
+++ lib/swag_dev/project/tasks/doc/watch.rb
@@ -16,11 +16,16 @@ end
 
 desc 'Watch documentation changes'
 task :'doc:watch' do
+  tools   = SwagDev.project.tools
   # ENV['LISTEN_GEM_DEBUGGING'] = '2'
-  watcher = SwagDev.project.tools.fetch(:yardoc_watcher)
+  watcher = tools.fetch(:yardoc_watcher)
 
-  begin
-    watcher.watch(true)
-  rescue SystemExit, Interrupt
+  tools.fetch(:process_locker).lock!(:doc_watch) do
+    # rubocop:disable Lint/HandleExceptions
+    begin
+      watcher.watch(true)
+    rescue SystemExit, Interrupt
+    end
+    # rubocop:enable Lint/HandleExceptions
   end
 end
