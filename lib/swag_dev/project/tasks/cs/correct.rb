0eb66495182905d3f6dc0cdce935d7b6155715b4
Dimitri Arrigoni <dimitri@arrigoni.me>
2017-12-26 14:04:13 +0100

    cs/correct (tasks) task name is ``cs:correct``

--- lib/swag_dev/project/tasks/cs/correct.rb
+++ lib/swag_dev/project/tasks/cs/correct.rb
@@ -3,6 +3,6 @@
 require_relative '../cs'
 
 desc 'Run static code analyzer, auto-correcting offenses'
-task 'cs:control', [:path] do |t, args|
+task 'cs:correct', [:path] do |t, args|
   rubocop(args[:path], '--fail-level', 'E', '--auto-correct')
 end
