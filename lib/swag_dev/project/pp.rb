69baa546f4a0bf75b8f4c9d176d2d0cbd02eebb1
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-01-03 19:24:26 +0100

    pp (project) add on Object

--- lib/swag_dev/project/pp.rb
+++ lib/swag_dev/project/pp.rb
@@ -2,4 +2,14 @@
 
 require 'swag_dev/project/concern/debug'
 
-self.extend(SwagDev::Project::Concern::Debug)
+# Object is the default root of all Ruby objects
+#
+# Object inherits from BasicObject
+# which allows creating alternate object hierarchies.
+# Methods on Object are available to all classes
+# (unless explicitly overridden).
+#
+# @see https://ruby-doc.org/core-2.5.0/Object.html
+class Object
+  include SwagDev::Project::Concern::Debug
+end
