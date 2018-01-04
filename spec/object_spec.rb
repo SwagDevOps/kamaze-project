6373e33a8ea6da1c170edfeb30d03dbabfbab547
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-01-04 19:05:27 +0100

    object (spec) tags added

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -3,7 +3,7 @@
 env = ENV['PROJECT_MODE'] || 'development'
 
 if 'development' == env
-  describe Object do
+  describe Object, :core_ext, :'core_ext/object', :development do
     [1, 2, 3].each do |n|
       it { expect(subject).to respond_to(:pp).with(n).arguments }
     end
