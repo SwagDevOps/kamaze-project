6bbba9e78af7c17ff081b663d3aa6a690aa2bb59
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-01-07 12:50:32 +0100

    concern/env (spec/factories) files sequence added

--- spec/factories/concern/env.rb
+++ spec/factories/concern/env.rb
@@ -1,5 +1,15 @@
-# -*- coding: utf-8 -*-
 # frozen_string_literal: true
+
+# Files are described by path and data (content loaded as env)
+sample_files = {
+  math: [
+    SAMPLES_PATH.join('concern/env/math.env').realpath,
+    {
+      'MATH_E' => Math::E.to_s,
+      'MATH_PI' => Math::PI.to_s
+    }
+  ]
+}
 
 FactoryBot.define do
   factory 'concern/env', class: FactoryStruct do
@@ -9,6 +19,18 @@ FactoryBot.define do
       @subjects[seq] = Class.new do
         include SwagDev::Project::Concern::Env
       end.new
+    end
+
+    sequence(:files) do |seq|
+      @files ||= []
+
+      sample_files.each do |k, v|
+        @files
+          .tap { |h| h[seq] = {} } [seq]
+          .merge!(k => FactoryStruct.new(path: v.fetch(0), data: v.fetch(1)))
+      end
+
+      @files[seq]
     end
   end
 end
