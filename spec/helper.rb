fa152f13507674db66bfe655e714b218482cbffd
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-01-08 08:37:18 +0100

    helper (spec) rake.rb used

--- spec/helper.rb
+++ spec/helper.rb
@@ -7,7 +7,5 @@
   :configure,
 ].each { |s| require [__dir__, 'helper', s].map(&:to_s).join('/') }
 
-require "#{__dir__}/../lib/swag_dev-project"
-# same context for ``rspec`` and ``rake``
-require 'rake'
-load("#{__dir__}/../Rakefile")
+require_relative '../lib/swag_dev-project'
+require_relative '../rake'
