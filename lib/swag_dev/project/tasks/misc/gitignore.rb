0b5fb878da76f8da1b4199042d7122034c9cfe32
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-01-10 08:25:31 +0100

    misc/gitignore (tasks) introduced

---
+++ lib/swag_dev/project/tasks/misc/gitignore.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'pathname'
+
+::Pathname.new('.gitignore').tap do |file|
+  if file.exist?
+    # desc "Reformat #{file} file"
+    task file.to_s do
+      content = file
+                  .read.lines
+                  .sort_by(&:downcase)
+                  .map(&:rstrip)
+                  .reject(&:empty?)
+                  .reject { |m| m[0] == '#' }
+                  .join("\n")
+
+      file.write(content)
+    end
+  end
+end
