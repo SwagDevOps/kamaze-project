fef1d8fb55da71e993155f154e10d7673ff73e96
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-01-23 19:16:23 +0100

    git (tools) almost a wip

--- lib/swag_dev/project/tools/git.rb
+++ lib/swag_dev/project/tools/git.rb
@@ -1,32 +1,27 @@
 # frozen_string_literal: true
 
 require_relative '../tools'
-require 'open3'
+require_relative 'base_tool'
 require 'cliver'
 
 # rubocop:disable Style/Documentation
 class SwagDev::Project::Tools
-  class Git
+  class Git < BaseTool
     class Command
-      protected
-
-      def executable
-        SwagDev::Project::Tools::Git.executable
-      end
-
-      def command
-        [executable]
-      end
-    end
-
-    class Status < Command
       class Buffer
       end
-
-      require_relative 'git/status'
     end
+    require_relative 'git/command'
+
+    class Status < Command
+    end
+    require_relative 'git/status'
+    # require_relative 'git/status/buffer'
 
     class << self
+      # Path to executable (binary}
+      #
+      # @return [String]
       # @raise Cliver::Dependency::NotFound
       def executable
         Cliver.detect(:git)
@@ -35,3 +30,29 @@ class SwagDev::Project::Tools
   end
 end
 # rubocop:enable Style/Documentation
+
+# Provide some git related features
+class SwagDev::Project::Tools::Git
+  # @return [String]
+  attr_accessor :executable
+
+  # Instance attributes altered during initialization
+  #
+  # @return [Array<Symbol>]
+  def mutable_attributes
+    [:executable]
+  end
+
+  # Get status
+  #
+  # @return [Status]
+  def status
+    Status.new(executable)
+  end
+
+  protected
+
+  def setup
+    @executable ||= self.class.executable
+  end
+end
