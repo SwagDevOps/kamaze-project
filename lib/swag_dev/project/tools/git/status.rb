0868777cb477dfd1e7cef4189cc2d0ec2fa58847
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-01-26 08:29:07 +0100

    git (tools) command, status, status/buffer introduced

---
+++ lib/swag_dev/project/tools/git/status.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require_relative 'command'
+
+# Provide status
+class SwagDev::Project::Tools::Git::Status
+  def parse!
+    @parsed = buffer.parse
+
+    self
+  end
+
+  def to_h
+    parsed
+  end
+
+  protected
+
+  attr_reader :buffer
+
+  # Prepare buffer string
+  def setup
+    @buffer = Buffer.new(capture_buffer).freeze
+    @parsed = nil
+  end
+
+  def parsed
+    @parsed ||= proc do
+      parse!
+      @parsed
+    end.call.freeze
+
+    @parsed
+  end
+
+  def command
+    super.push('status', '-z', '--untracked-files=all')
+  end
+
+  def capture_buffer
+    capture[0].to_s.lines.map(&:chomp).join("\n")
+  end
+end
---
+++ lib/swag_dev/project/tools/git/status/buffer.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+require_relative '../status'
+require 'pathname'
+
+# Buffer
+class SwagDev::Project::Tools::Git::Status::Buffer
+  def initialize(content)
+    @content = content.tr("\000", "\n").freeze
+  end
+
+  def to_s
+    content
+  end
+
+  # Get matchers
+  #
+  # @return [Hash{Symbol => Regexp}]
+  def matchers
+    {
+      staged: /^[A-Z]/,
+      unstaged: /^\s/,
+      untracked: /^\?/,
+    }
+  end
+
+  # Parse output
+  #
+  # @return [{Symbol => String}]
+  def parse
+    output = matchers.map { |k, v| [k, []] }.to_h
+
+    preparse.each do |k, v|
+      v.each do |filepath|
+        add_filepath(output, filepath, k)
+      end
+    end
+
+    output
+  end
+
+  # Fast parse to a ``Hash`` (naive)
+  #
+  # @return [{String => String}]
+  def preparse
+    parsed = {}
+    # git status -z --untracked-files=all | sed "s/\x0/\n/g"
+    self.to_s.lines.each do |line|
+      match = /^([M|A|R|C|D|U|\?|\s]{2})[ ]{1}(.*)/.match(line)
+
+      (parsed[match[1]] ||= []).push(match[2]) unless match.nil?
+    end
+
+    parsed
+  end
+
+  protected
+
+  attr_reader :content
+
+  # Add filepath on output using flag
+  #
+  # @param [Hash] result where files are stored by type
+  # @param [String] filepath
+  # @param [String] flag
+  # @return [nil|Object]
+  #
+  # @see https://git-scm.com/docs/git-status
+  def add_filepath(result, filepath, flag)
+    file = Struct.new(:path, :base, :flag)
+    base = ::Pathname.new(Dir.pwd)
+
+    matchers.each do |type, reg|
+      next unless reg =~ flag
+
+      return result[type] << file.new(filepath.to_s, base, flag.to_s).freeze
+    end
+  end
+end
