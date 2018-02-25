4a288d44e3cd5cfe68826691887f3c5df176fbe2
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-02-25 12:34:57 +0100

    object (spec) according to previous changes #e5f25b0

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -4,8 +4,16 @@ env = ENV['PROJECT_MODE'] || 'development'
 
 if 'development' == env
   describe Object, :core_ext, :'core_ext/object', :development do
-    [1, 2, 3].each do |n|
-      it { expect(subject).to respond_to(:pp).with(n).arguments }
+    let(:subject) do
+      # ``pp`` method SHOULD be ``private``
+      subject = described_class.new
+      subject.singleton_class.class_eval { public 'pp' }
+
+      subject
     end
+
+    it { expect(subject).to respond_to(:pp) }
+    it { expect(subject).to respond_to(:pp).with(0).arguments }
+    it { expect(subject).to respond_to(:pp).with_unlimited_arguments }
   end
 end
