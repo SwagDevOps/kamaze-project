0aea6ab012944ec6623636e9bec3fce6d735077f
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-03-02 17:50:53 +0100

    sources/license (tasks) handle SystemExit, Interrupt, Errno::EPIPE

--- lib/swag_dev/project/tasks/sources/license.rb
+++ lib/swag_dev/project/tasks/sources/license.rb
@@ -4,18 +4,20 @@ require_relative '../sources'
 
 desc 'Apply license on (ruby) source files'
 task 'sources:license', [:output] => ['gem:gemspec'] do |task, args|
-  output   = args[:output] ? Pathname.new(args[:output]) : nil
+  output  = args[:output] ? Pathname.new(args[:output]) : nil
+  project = SwagDev.project
+  builder = project.tools.fetch(:gemspec_builder)
 
-  project  = SwagDev.project
-  licenser = project.tools.fetch(:licenser)
-  builder  = project.tools.fetch(:gemspec_builder)
-
-  licenser.process do |process|
-    process.output  = output if output
-    process.license = project.version_info.fetch(:license_header)
-    process.files   = builder.source_files.select do |file|
-      # @todo use a better ruby files recognition
-      file.extname.gsub(/^\./, '') == 'rb'
+  begin
+    project.tools.fetch(:licenser).process do |process|
+      process.output  = output if output
+      process.license = project.version_info.fetch(:license_header)
+      process.files   = builder.source_files.select do |file|
+        # @todo use a better ruby files recognition
+        file.extname.gsub(/^\./, '') == 'rb'
+      end
     end
+  rescue SystemExit, Interrupt, Errno::EPIPE => e
+    exit(e.class.const_get(:Errno)) if e.is_a?(Errno::EPIPE)
   end
 end
