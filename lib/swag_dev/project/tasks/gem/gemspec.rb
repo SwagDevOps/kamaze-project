4c06ab74e5463af8e45587cda7b687d61f3de2ad
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-03-02 11:39:55 +0100

    gem/gemspec (tasks) fix offenses + add some comments

--- lib/swag_dev/project/tasks/gem/gemspec.rb
+++ lib/swag_dev/project/tasks/gem/gemspec.rb
@@ -5,13 +5,12 @@ require_relative '../gem'
 tools  = SwagDev.project.tools
 reader = tools.fetch(:gemspec_reader)
 writer = tools.fetch(:gemspec_writer)
-# dependency files
-files  = [writer.templated]
-                .concat((reader.read&.files).to_a)
-                .concat(Dir.glob(['gems.rb', 'gems.locked']))
-                .map(&:to_s)
-                .sort
+# project files ------------------------------------------------------
+files  = [writer.templated].concat((reader.read&.files).to_a)
+                           .concat(Dir.glob(['gems.rb', 'gems.locked']))
+                           .map(&:to_s).sort
 
+# task ---------------------------------------------------------------
 desc 'Update gemspec'
 task 'gem:gemspec': [writer.to_s]
 file writer.to_s => files do
