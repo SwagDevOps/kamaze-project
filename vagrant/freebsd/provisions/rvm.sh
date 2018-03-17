a1043acf3d7f1799cc9ba1e2b7bd92546262a87b
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-03-17 18:32:08 +0100

    freebsd (vagrant) changes and improvements + refactor

--- vagrant/freebsd/provisions/rvm.sh
+++ vagrant/freebsd/provisions/rvm.sh
@@ -1,15 +1,12 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 export RUBY_VERSION=2.3.3
 
-# RVM installation ---------------------------------------------------
-test -f '/usr/local/rvm/bin/rvm' || {
-    curl -sSL 'https://get.rvm.io' | bash -s 'stable'
-}
+set -e
 
-bash << RVM_INSTALL
+# RVM installation ---------------------------------------------------
+curl -sSL 'https://get.rvm.io' | bash -s 'stable'
 . /etc/profile.d/rvm.sh
-export BUNDLE_SILENCE_ROOT_WARNING=1
 
 rvm use "${RUBY_VERSION}" || {
     rvm install "${RUBY_VERSION}"
@@ -19,7 +16,6 @@ rvm use "${RUBY_VERSION}" || {
 gem install --conservative    \
             --no-user-install \
             --no-post-install-message bundler
-RVM_INSTALL
 
 ln -sfv \
    "/usr/local/rvm/rubies/ruby-${RUBY_VERSION}/bin/ruby" \
