2a439fb222a8cca53fed8bd81fc409e921876b5c
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-03-19 09:01:05 +0100

    console/output/buffer (tools) fix rubocop offense + documentation added

--- lib/swag_dev/project/tools/console/output/buffer.rb
+++ lib/swag_dev/project/tools/console/output/buffer.rb
@@ -10,6 +10,9 @@ end
 
 # Describe a buffer
 class SwagDev::Project::Tools::Console::Output::Buffer
+  # Get content
+  #
+  # @return [String]
   attr_reader :content
 
   # @param [SwagDev::Project::Tools::Console::Output] output
@@ -19,17 +22,21 @@ class SwagDev::Project::Tools::Console::Output::Buffer
     @content = content
   end
 
+  # @return [String]
   def to_s
     self.decorate(content)
   end
 
   protected
 
+  # @return [SwagDev::Project::Tools::Console::Output]
   attr_reader :output
 
-  # @param [String] s
+  # Decorate given string depending ``output`` is a tty
+  #
+  # @param [String] str
   # @return [String]
-  def decorate(s)
-    CLI::UI.fmt(s, enable_color: output.tty?)
+  def decorate(str)
+    CLI::UI.fmt(str.to_s, enable_color: output.tty?)
   end
 end
