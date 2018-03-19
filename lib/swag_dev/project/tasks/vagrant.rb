a94f3c24cb72b6964d7f7ae67576ed9a417de12c
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-03-19 08:40:11 +0100

    vagrant (tasks) minor changes and improvements

--- lib/swag_dev/project/tasks/vagrant.rb
+++ lib/swag_dev/project/tasks/vagrant.rb
@@ -3,6 +3,7 @@
 require 'swag_dev/project'
 require 'rake/clean'
 require 'shellwords'
+require 'yaml'
 
 vagrant = SwagDev.project.tools.fetch(:vagrant)
 
@@ -32,25 +33,25 @@ task 'vagrant:init': ['Vagrantfile']
 end
 
 desc 'Dump config'
-task 'vagrant:dump', [:output] => ['vagrant:init'] do |task, args|
-  out_file = Pathname.new(args[:output] || '/dev/stdout')
-  dump = vagrant.dump
+task 'vagrant:dump', [:box_id] => ['vagrant:init'] do |task, args|
+  dump = args[:box_id] ? vagrant.boxes.fetch(args[:box_id]) : vagrant.boxes
 
-  Pathname.new(out_file).write(dump)
+  $stdout.puts(YAML.dump(dump))
 end
 
 # :vagrant:vm --------------------------------------------------------
 
-vagrant.boxes.each do |box, box_config|
+vagrant.boxes.each_key do |box_id|
+  task_ns = "vagrant:vm:#{box_id}"
   [:up, :halt, :reload, :provision, :rsync].each do |command|
-    desc "#{command.to_s.gsub(/(\w+)/, &:capitalize)} #{box}"
-    task "vagrant:vm:#{box}:#{command}": ['vagrant:init'] do
-      vagrant.execute(command.to_s, box)
+    desc "#{command.to_s.gsub(/(\w+)/, &:capitalize)} #{box_id}"
+    task "#{task_ns}:#{command}": ['vagrant:init'] do
+      vagrant.execute(command.to_s, box_id)
     end
   end
 
-  desc "Ssh #{box}"
-  task "vagrant:vm:#{box}:ssh", [:command] => ['vagrant:init'] do |task, args|
-    vagrant.ssh(box, args[:command])
+  desc "Ssh #{box_id}"
+  task "#{task_ns}:ssh", [:command] => ['vagrant:init'] do |task, args|
+    vagrant.ssh(box_id, args[:command])
   end
 end
