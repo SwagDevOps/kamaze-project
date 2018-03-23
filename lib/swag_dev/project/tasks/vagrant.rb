309d6eb0d37acda3e35c6ea9e4d352e2b1599468
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-03-23 19:15:58 +0100

    vagrant (tasks) minor changes and improvements

--- lib/swag_dev/project/tasks/vagrant.rb
+++ lib/swag_dev/project/tasks/vagrant.rb
@@ -22,13 +22,10 @@ task 'vagrant:init': ['Vagrantfile']
   'status': 'Current machine states',
 }.each do |k, v|
   desc v
-  task "vagrant:#{k}": ['vagrant:init'] do
-    begin
-      vagrant.execute(k.to_s)
-      # rubocop:disable Lint/HandleExceptions
-    rescue SystemExit, Interrupt
-      # rubocop:enable Lint/HandleExceptions
-    end
+  task "vagrant:#{k}" => ['vagrant:init'] do |task|
+    vagrant.execute(k.to_s)
+
+    task.reenable
   end
 end
 
@@ -37,21 +34,33 @@ task 'vagrant:dump', [:box_id] => ['vagrant:init'] do |task, args|
   dump = args[:box_id] ? vagrant.boxes.fetch(args[:box_id]) : vagrant.boxes
 
   $stdout.puts(YAML.dump(dump))
+
+  task.reenable
 end
 
 # :vagrant:vm --------------------------------------------------------
 
 vagrant.boxes.each_key do |box_id|
   task_ns = "vagrant:vm:#{box_id}"
-  [:up, :halt, :reload, :provision, :rsync].each do |command|
+  {
+    up: [:halt],
+    halt: [:up],
+    reload: [:reload, :halt],
+    provision: [:provision],
+    rsync: [:rsync]
+  }.each do |command, tasks|
     desc "#{command.to_s.gsub(/(\w+)/, &:capitalize)} #{box_id}"
-    task "#{task_ns}:#{command}": ['vagrant:init'] do
+    task "#{task_ns}:#{command}" => ['vagrant:init'] do
       vagrant.execute(command.to_s, box_id)
+
+      tasks.each { |tid| Rake::Task["#{task_ns}:#{tid}"].reenable }
     end
   end
 
   desc "Ssh #{box_id}"
   task "#{task_ns}:ssh", [:command] => ['vagrant:init'] do |task, args|
     vagrant.ssh(box_id, args[:command])
+
+    task.reenable
   end
 end
