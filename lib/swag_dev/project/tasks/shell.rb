9a57c709f1249fa674cebb9dab1af8028985f461
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-03-29 08:18:38 +0200

    shell (tasks) shell tool used

--- lib/swag_dev/project/tasks/shell.rb
+++ lib/swag_dev/project/tasks/shell.rb
@@ -1,32 +1,13 @@
 # frozen_string_literal: true
 
-# Provides a task intented to easily start pry REPL
+# Start a REPL
 #
-# * more convenient than ``bundle exec pry``
-# * displays ``RUBY_DESCRIPTION`` (variant) on startup
-
-tools  = SwagDev.project.tools
-
-# banner -------------------------------------------------------------
-banner = lambda do
-  patch = defined?(RUBY_PATCHLEVEL) ? "p#{RUBY_PATCHLEVEL}" : nil
-  # almost like RUBY_DESCRIPTION
-  descr = ["#{RUBY_ENGINE} #{RUBY_VERSION}#{patch}".rstrip,
-           "(#{RUBY_RELEASE_DATE})",
-           "[#{RUBY_PLATFORM}]"].join(' ')
-
-  tools.fetch(:console).stdout.puts("{{green:#{descr}}}")
-end
-
-# shell --------------------------------------------------------------
-shell = lambda do
-  require 'pry'
-
-  Pry.start
-end
+# @see [SwagDev::Project::Tools::Shell]
 
 # task ---------------------------------------------------------------
 desc 'Start ruby REPL'
 task :shell do
-  [banner, shell].each(&:call)
+  shell = SwagDev.project.tools.fetch(:shell)
+
+  shell.start
 end
