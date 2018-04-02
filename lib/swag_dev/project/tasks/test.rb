79e4611fc190aa91b1af3644852a06861e646e7c
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-04-02 21:06:18 +0200

    test (tasks) use DSL methods

--- lib/swag_dev/project/tasks/test.rb
+++ lib/swag_dev/project/tasks/test.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
-require_relative '../../project'
-
-# https://www.relishapp.com/rspec/rspec-core/docs/command-line/rake-task
-# https://github.com/rspec/rspec-core/blob/master/lib/rspec/core/runner.rb
-# https://relishapp.com/rspec/rspec-core/v/2-4/docs/command-line/tag-option
 desc 'Run test suites'
 task :test, [:tags] do |task, args|
-  SwagDev.project.tools.fetch(:rspec).tap do |rspec|
+  tools.fetch(:rspec).tap do |rspec|
     rspec.tags = args[:tags].to_s.split(',').map(&:strip)
   end.run
 end
