d5c662368c6e32db5fa5c9cdec8c36a1e876d439
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-04-02 16:03:06 +0200

    rspec (tools) introduced

---
+++ lib/swag_dev/project/tools/rspec.rb
@@ -0,0 +1,109 @@
+# frozen_string_literal: true
+
+require_relative '../tools'
+require_relative 'base_tool'
+
+class SwagDev::Project::Tools
+  class Rspec < BaseTool
+  end
+end
+
+# Provide wrapper based on ``rspec``
+#
+# Intended to easyfy
+class SwagDev::Project::Tools::Rspec
+  # Default arguments used by ``RSpec::Core::Runner``
+  #
+  # @type [Array|Arguments]
+  # @return [Arguments]
+  attr_accessor :defaults
+
+  attr_accessor :stdout
+
+  attr_accessor :stderr
+
+  # @return [Array<String>]
+  attr_accessor :tags
+
+  # @type [Boolean]
+  # @return [Boolean]
+  attr_accessor :fail_on_error
+
+  def mutable_attributes
+    [:defaults, :stdout, :stderr, :fail_on_error]
+  end
+
+  # Reset arguments
+  #
+  # @return [self]
+  def reset
+    @arguments = nil
+
+    self
+  end
+
+  def run
+    options = arguments.concat(options_arguments).map(&:to_s)
+    retcode = core.run(options, stderr, stdout).to_i
+
+    reset.on_error(retcode)
+  end
+
+  # Arguments used by ``CLI`` (during execution/``run``)
+  #
+  # @return [Arguments]
+  def arguments
+    @arguments = @defaults if @arguments.to_a.size.zero?
+
+    tags.to_a.each do |tag|
+      next if @arguments.slice_before('--tag').to_a.include?(['--tag', tag])
+
+      @arguments += ['--tag', tag]
+    end
+
+    {
+      true  => @arguments,
+      false => @arguments.clone.concat(options_arguments).map(&:to_s).freeze
+    }.fetch(caller_locations(1..1).first.path == __FILE__)
+  end
+
+  protected
+
+  def setup
+    reset
+
+    @tags = []
+    @stdout ||= STDOUT
+    @stderr ||= STDERR
+    @defaults ||= []
+    @fail_on_error = true if @fail_on_error.nil?
+  end
+
+  # @return [RSpec::Core::Runner]
+  def core
+    require 'rspec/core'
+
+    RSpec::Core::Runner
+  end
+
+  # Abort execution on error
+  #
+  # @param [Fixnum] code
+  # @return [Fixnum]
+  def on_error(code)
+    (exit(code) if fail_on_error?) unless code.zero?
+
+    code
+  end
+
+  def options_arguments
+    options_files = Pathname.new(Dir.pwd).join('.rspec')
+    defaults = []
+
+    if options_files.file? and options_files.readable?
+      defaults += ['-O', '.rspec']
+    end
+
+    defaults
+  end
+end
