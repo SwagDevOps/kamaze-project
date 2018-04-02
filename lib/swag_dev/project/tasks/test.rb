9d7b4aceb94f7a5b6ff2b2dd009ec9d13883365f
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-04-02 18:28:58 +0200

    test (tasks) rspec tool used

--- lib/swag_dev/project/tasks/test.rb
+++ lib/swag_dev/project/tasks/test.rb
@@ -2,31 +2,12 @@
 
 require_relative '../../project'
 
-# More convenient than ``bundle exec``
-#
 # https://www.relishapp.com/rspec/rspec-core/docs/command-line/rake-task
 # https://github.com/rspec/rspec-core/blob/master/lib/rspec/core/runner.rb
 # https://relishapp.com/rspec/rspec-core/v/2-4/docs/command-line/tag-option
 desc 'Run test suites'
 task :test, [:tags] do |task, args|
-  proc do
-    require 'rspec/core'
-    require 'shellwords'
-
-    spec_runner = RSpec::Core::Runner
-    conf_file = Pathname.new(Dir.pwd).join('.rspec')
-    optionner = lambda do |file|
-      file.file? ? Shellwords.split(file.read) : []
-    end
-
-    options = optionner.call(conf_file)
-    args[:tags]
-      .to_s
-      .split(',')
-      .map(&:strip).each { |tag| options += ['--tag', tag] }
-
-    status = spec_runner.run(options, STDERR, STDOUT).to_i
-
-    exit(status) unless status.zero?
-  end.call
+  SwagDev.project.tools.fetch(:rspec).tap do |rspec|
+    rspec.tags = args[:tags].to_s.split(',').map(&:strip)
+  end.run
 end
