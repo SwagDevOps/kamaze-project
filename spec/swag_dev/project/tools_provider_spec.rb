38599a28e56818ed9f1a114faa594a4980280a47
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-04-09 08:51:56 +0200

    tools_provider (spec) introduced

---
+++ spec/swag_dev/project/tools_provider_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'swag_dev/project/tools_provider'
+
+describe SwagDev::Project::ToolsProvider, :tools_provider do
+  it { expect(subject).to respond_to(:to_h).with(0).arguments }
+  it { expect(subject).to respond_to(:get).with(1).arguments }
+  it { expect(subject).to respond_to(:fetch).with(1).arguments }
+  it { expect(subject).to respond_to('[]').with(1).arguments }
+  it { expect(subject).to respond_to('member?').with(1).arguments }
+end
+
+describe SwagDev::Project::ToolsProvider, :tools_provider do
+  context '.defaults' do
+    build('tools').keys.each do |k|
+      it { expect(described_class.defaults.keys).to include(k) }
+    end
+  end
+
+  context '#to_h' do
+    it { expect(subject.to_h).to be_a(Hash) }
+  end
+
+  context '#to_h.keys' do
+    build('tools').keys.each do |k|
+      it { expect(subject.to_h.keys).to include(k) }
+    end
+  end
+end
