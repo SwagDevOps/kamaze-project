8555f21f20a88070f768693c52a76674ee3bc31e
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-04-10 18:59:50 +0200

    tools/base_tool (spec/factories) removed

--- spec/factories/tools/base_tool.rb
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory 'tools/base_tool', class: FactoryStruct do
-    # Describe instance methods
-    describe_instance_methods({ mutable_attributes: [0] })
-  end
-end
