d4c5652f6fcceb519a0df8d58b6facfa8e11223b
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-04-10 19:14:32 +0200

    tools/console (spec) rewrite + update examles

--- spec/swag_dev/project/tools/console_spec.rb
+++ spec/swag_dev/project/tools/console_spec.rb
@@ -2,15 +2,7 @@
 
 require 'swag_dev/project/tools/console'
 
-describe SwagDev::Project::Tools::Console do
-  build('tools/console')
-    .describe_instance_methods
-    .each do |method, counts|
-    counts.each do |n|
-      it { expect(subject).to respond_to(method).with(n).arguments }
-    end
-  end
-
+describe SwagDev::Project::Tools::Console, :tools, :'tools/console' do
   context '.new' do
     it do
       base_class = SwagDev::Project::Tools::BaseTool
@@ -20,14 +12,14 @@ describe SwagDev::Project::Tools::Console do
   end
 end
 
-describe SwagDev::Project::Tools::Console do
-  context '#mutable_attributes' do
-    [:stdout, :stderr].each do |attr|
+describe SwagDev::Project::Tools::Console, :tools, :'tools/console' do
+  [:stdout, :stderr].each do |attr|
+    it { expect(subject).to respond_to(attr).with(0).arguments }
+
+    context '#mutable_attributes' do
       it { expect(subject.mutable_attributes).to include(attr) }
     end
-  end
 
-  [:stdout, :stderr].each do |attr|
     context "##{attr}" do
       it do
         output_class = SwagDev::Project::Tools::Console::Output
