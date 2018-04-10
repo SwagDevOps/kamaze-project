9d6ce46f6c00961799ccd189245fc4cfc8b3243e
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-04-10 09:09:34 +0200

    tools_provider (spec) tests added

--- spec/swag_dev/project/tools_provider_spec.rb
+++ spec/swag_dev/project/tools_provider_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'swag_dev/project/tools_provider'
+require 'securerandom'
 
 describe SwagDev::Project::ToolsProvider, :tools_provider do
   it { expect(described_class).to respond_to(:defaults).with(0).arguments }
@@ -27,6 +28,25 @@ describe SwagDev::Project::ToolsProvider, :tools_provider do
   context '#to_h.keys' do
     build('tools').keys.each do |k|
       it { expect(subject.to_h.keys).to include(k) }
+    end
+  end
+
+  build('tools').keys do |k|
+    context "#to_h[#{k}]" do
+      it { subject.to_h[k].to be_a(Class) }
+    end
+  end
+end
+
+describe SwagDev::Project::ToolsProvider, :tools_provider do
+  context '#[]' do
+    it { expect(subject["fake_#{SecureRandom.hex[0..8]}"]).to be(nil) }
+  end
+
+  context '#fetch()' do
+    it do
+      expect { subject.fetch("fake_#{SecureRandom.hex[0..8]}") }
+        .to raise_error(KeyError)
     end
   end
 end
