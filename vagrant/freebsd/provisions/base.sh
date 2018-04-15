c7660275663198408b11250fcce5b760a5e703cf
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-04-15 18:36:35 +0200

    freebsd/provisions/base (vagrant) changes and improvements

--- vagrant/freebsd/provisions/base.sh
+++ vagrant/freebsd/provisions/base.sh
@@ -1,27 +1,39 @@
 #!/usr/bin/env sh
-
-# minimal rbenv requirements -----------------------------------------
-export PACKAGES='bash bash-completion'
-# misc utils ---------------------------------------------------------
-PACKAGES="${PACKAGES} htop pstree pidof curl vim-lite direnv"
-# libgit2/rugged  ----------------------------------------------------
-PACKAGES="${PACKAGES} cmake devel/pkgconf"
+set -e
 
 # packages installation ----------------------------------------------
+# bash + misc utils + libgit2/rugged
+PACKAGES=$(cat <<'EOF'
+bash bash-completion
+curl htop pstree pidof vim-console
+cmake devel/pkgconf
+EOF
+)
+
 pkg update
 for i in $(echo $PACKAGES | perl -pe "s#\s+#\n#g" | sort -u); do
-    printf "Installing %s\n" "$i"
-    pkg install -Uy "$i"
+    printf "Installing %s ...\n" "$i"
+    pkg install -Uy "$i" || exit 95 # EOPNOTSUPP
 done
 
 # bash setup ---------------------------------------------------------
-chsh -s '/usr/local/bin/bash' 'vagrant'
-echo '. /etc/profile 2>/dev/null' > /home/vagrant/.profile
-echo '. "${HOME}/.bashrc" 2>/dev/null' >> /home/vagrant/.profile
+(set -e
+ cd /home/vagrant
+
+ chsh -s '/usr/local/bin/bash' 'vagrant'
+
+ tee .profile <<EOF > /dev/null
+. /etc/profile 2>/dev/null
+. "\${HOME}/.bashrc" 2>/dev/null
+EOF
+)
+
+# .gemrc -------------------------------------------------------------
 for i in /home/vagrant /root; do
     echo 'gem: --no-ri --no-rdoc' > "${i}/.gemrc"
 done
 
+# permissions + misc  ------------------------------------------------
 chown -Rf 'vagrant:vagrant' '/home/vagrant'
-rm -rf /home/*/*.iso
+rm -f /home/*/*.iso
 head -1 /etc/motd | tee /etc/motd
