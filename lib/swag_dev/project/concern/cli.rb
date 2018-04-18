0554102281487f354c258b739996c7656858d7ad
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-04-18 09:38:29 +0200

    cli (concern) introduced

---
+++ lib/swag_dev/project/concern/cli.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative '../concern'
+require 'active_support/concern'
+
+# Concern for CLI
+module SwagDev::Project::Concern::Cli
+  extend ActiveSupport::Concern
+
+  # @!attribute [r] retcode
+  #   @return [Fixnum] retcode
+
+  included do
+    class_eval <<-"ACCESSORS", __FILE__, __LINE__ + 1
+        protected
+
+        attr_writer :retcode
+    ACCESSORS
+  end
+
+  # @return [Fixnum]
+  def retcode
+    @retcode ||= 0
+  end
+
+  # Denote execution is a success.
+  #
+  # @return [Boolean]
+  def success?
+    retcode.zero?
+  end
+end
