35191ff2e64ced76ec68794c2ef60ba99f1bdcdb
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-05-15 08:31:51 +0200

    object (spec) examples added for pp

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -15,5 +15,24 @@ if 'development' == env
     it { expect(subject).to respond_to(:pp) }
     it { expect(subject).to respond_to(:pp).with(0).arguments }
     it { expect(subject).to respond_to(:pp).with_unlimited_arguments }
+
+    # testing return type and value(s)
+    {
+      [] => nil.class,
+      [{}] => Hash,
+      [42] => Integer,
+      [42, 1337] => Array
+    }.each do |params, type|
+      context ".pp(*#{params})" do
+        before { allow($stdout).to receive(:write) }
+
+        it { expect(subject.pp(*params)).to be_a(type) }
+
+        it do
+          expect(subject.pp(*params))
+            .to eq(params.size <= 1 ? params[0] : params)
+        end
+      end
+    end
   end
 end
