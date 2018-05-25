0b8bb26684f697ba6c44682919bd9dd59eb3d4ba
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-05-25 18:14:52 +0200

    sources/license (tasks) project.version instead of const_get

--- lib/kamaze/project/tasks/sources/license.rb
+++ lib/kamaze/project/tasks/sources/license.rb
@@ -6,12 +6,11 @@ desc 'Apply license on (ruby) source files'
 task 'sources:license', [:output] => [writer.to_s] do |task, args|
   output  = args[:output] ? Pathname.new(args[:output]) : nil
   builder = tools.fetch(:gemspec_builder)
-  version = project.subject.const_get('VERSION')
 
   begin
     tools.fetch(:licenser).process do |process|
       process.output  = output if output
-      process.license = version.license_header
+      process.license = project.version.license_header
       process.files   = builder.source_files.select do |file|
         # @todo use a better ruby files recognition
         # @todo implement better exclusion
