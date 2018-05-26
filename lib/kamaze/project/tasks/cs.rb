4edf341e93e299b5a0cba9a01022a30eb1e7eaee
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-05-26 17:32:12 +0200

    apply license on source files

--- lib/kamaze/project/tasks/cs.rb
+++ lib/kamaze/project/tasks/cs.rb
@@ -14,4 +14,10 @@
 #   percy-style: [ default.yml ]
 # ~~~~
 
+# Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
+# License GPLv3+: GNU GPL version 3 or later
+# <http://www.gnu.org/licenses/gpl.html>.
+# This is free software: you are free to change and redistribute it.
+# There is NO WARRANTY, to the extent permitted by law.
+
 [:control, :correct].each { |req| require_relative "cs/#{req}" }
