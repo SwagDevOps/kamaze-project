2bf2d05354dc6bffa1fba4d8c04383a4d0deef11
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-05-28 19:28:15 +0200

    project (spec) introduced

---
+++ spec/kamaze/project_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'kamaze/project'
+
+# testing instance (methods)
+describe Kamaze::Project, :project do
+  let(:subject) { Kamaze.project }
+
+  it { expect(subject).to respond_to(:name).with(0).arguments }
+  it { expect(subject).to respond_to(:subject).with(0).arguments }
+  it { expect(subject).to respond_to(:version).with(0).arguments }
+  it { expect(subject).to respond_to('load!').with(0).arguments }
+  it { expect(subject).to respond_to(:mode).with(0).arguments }
+  it { expect(subject).to respond_to(:tasks).with(0).arguments }
+  it { expect(subject).to respond_to(:tools).with(0).arguments }
+
+  it { expect(subject).to respond_to(:path).with(0).arguments }
+  it { expect(subject).to respond_to(:path).with_unlimited_arguments }
+end
