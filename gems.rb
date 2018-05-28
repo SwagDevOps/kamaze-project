981a963020173b25b3b7824452640d3ffddcfaba
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-05-28 08:18:51 +0200

    relaxing rules in gems

--- gems.rb
+++ gems.rb
@@ -9,13 +9,13 @@ source 'https://rubygems.org'
 gem 'cli-ui', '~> 1.1'
 gem 'cliver', '~> 0.3'
 gem 'dotenv', '~> 2.4'
-gem 'dry-inflector', '~> 0.1', '< 0.2'
+gem 'dry-inflector', '~> 0.1'
 gem 'process_lock', '~> 0.1'
 gem 'rake', '~> 12.3'
 gem 'rugged', '~> 0.27'
 gem 'tenjin', '~> 0.7'
 gem 'tty-editor', '~> 0.4'
-gem 'tty-screen', '>= 0.6.2', '< 0.7'
+gem 'tty-screen', '>= 0.6.2', '~> 0.6'
 
 # supported tools ----------------------------------------------------
 
@@ -32,7 +32,7 @@ group :development do
   gem 'interesting_methods', '~> 0.1'
   gem 'pry', '~> 0.11'
   gem 'pry-coolline', '~> 0.2'
-  gem 'sys-proc', '~> 1.0', '>= 1.0.5'
+  gem 'sys-proc', '>= 1.0.5', '~> 1.0'
 
   # 'listen' is used to "watch"
   # but could be incompatible with some systems
