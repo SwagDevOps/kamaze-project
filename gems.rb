d5220da90fdc9351ccd7545a8c0c6f85cf76496a
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-05-31 08:54:44 +0200

    minor changes

--- gems.rb
+++ gems.rb
@@ -4,33 +4,30 @@
 
 source 'https://rubygems.org'
 
-# base ---------------------------------------------------------------
-
-gem 'cli-ui', '~> 1.1'
-gem 'cliver', '~> 0.3'
-gem 'dotenv', '~> 2.4'
-gem 'dry-inflector', '~> 0.1'
-gem 'process_lock', '~> 0.1'
-gem 'rake', '~> 12.3'
-gem 'rugged', '~> 0.27'
-gem 'tenjin', '~> 0.7'
-gem 'tty-editor', '~> 0.4'
-gem 'tty-screen', '>= 0.6.2', '~> 0.6'
-
-# supported tools ----------------------------------------------------
-
-gem 'rubocop', '~> 0.56'
-gem 'yard', '~> 0.9'
+group :default do
+  gem 'cli-ui', '~> 1.1'
+  gem 'cliver', '~> 0.3'
+  gem 'dotenv', '~> 2.4'
+  gem 'dry-inflector', '~> 0.1'
+  gem 'process_lock', '~> 0.1'
+  gem 'rake', '~> 12.3'
+  gem 'rugged', '~> 0.27'
+  gem 'tenjin', '~> 0.7'
+  gem 'tty-editor', '~> 0.4'
+  gem 'tty-screen', '>= 0.6.2', '~> 0.6'
+  # supported tools --------------------------------------------------
+  gem 'rubocop', '~> 0.56'
+  gem 'yard', '~> 0.9'
+end
 
 # --------------------------------------------------------------------
-# 'rspec' and 'pry' are also supported, but we don't force to use it
-
-# development, doc, test ---------------------------------------------
+# 'rspec' and 'pry' are also supported
+# but we don't force to use them
+# --------------------------------------------------------------------
 
 group :development do
   gem 'bootsnap', '~> 1.3'
   gem 'interesting_methods', '~> 0.1'
-  gem 'pry', '~> 0.11'
   gem 'pry-coolline', '~> 0.2'
   gem 'sys-proc', '~> 1.1'
 
@@ -41,6 +38,7 @@ end
 
 group :doc, :development do
   gem 'github-markup', '~> 2.0'
+  gem 'pry', '~> 0.11'
   gem 'redcarpet', '~> 3.4'
 end
 
