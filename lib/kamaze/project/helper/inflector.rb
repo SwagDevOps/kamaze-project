66f98ee893f3eafb1a1445ac3c520b0ab375f590
Dimitri Arrigoni <dimitri@arrigoni.me>
2018-06-06 21:04:53 +0200

    inflector (helper) require_relative

--- lib/kamaze/project/helper/inflector.rb
+++ lib/kamaze/project/helper/inflector.rb
@@ -6,7 +6,7 @@
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
 
-require 'kamaze/project/helper'
+require_relative '../helper'
 
 # Inflector built on top of ``Dry::Inflector``
 class Kamaze::Project::Helper::Inflector
