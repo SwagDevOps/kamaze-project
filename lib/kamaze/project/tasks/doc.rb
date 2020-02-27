ed3568164e549e32e9e49883f0fd6e1d12e8466d
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-02-28 00:32:47 +0100

    update license headers in lib
    ```
    find lib/ | grep -E '.*\.rb$' | while read line; do cat "$line" | perl -pe 's/Copyright \(C\) 2017-2018/Copyright (C) 2017-2020/g' | sponge "$line"; done
    ```

--- lib/kamaze/project/tasks/doc.rb
+++ lib/kamaze/project/tasks/doc.rb
@@ -1,7 +1,7 @@
 # coding: utf-8
 # frozen_string_literal: true
 
-# Copyright (C) 2017-2018 Dimitri Arrigoni <dimitri@arrigoni.me>
+# Copyright (C) 2017-2020 Dimitri Arrigoni <dimitri@arrigoni.me>
 # License GPLv3+: GNU GPL version 3 or later
 # <http://www.gnu.org/licenses/gpl.html>.
 # This is free software: you are free to change and redistribute it.
