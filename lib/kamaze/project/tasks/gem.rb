d96fe79c771ef6e11d12d4ca421cf9cc0070891f
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-03-08 12:03:35 +0100

    gem (tasks) generate gemspec file when file is missing

--- lib/kamaze/project/tasks/gem.rb
+++ lib/kamaze/project/tasks/gem.rb
@@ -6,6 +6,10 @@
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
 
+tools.fetch(:gemspec_writer).tap do |writer|
+  writer.write unless writer.generated.file?
+end
+
 # Require base tasks ------------------------------------------------
 [:gemspec, :build].each { |req| require_relative "gem/#{req}" }
 
