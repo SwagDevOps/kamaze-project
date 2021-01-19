de241b4dbc008e4b510771fb1a398ba27cdfa4c5
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-19 21:04:47 +0100

    pp (core_ext) Kernel -> Object

--- lib/kamaze/project/core_ext/pp.rb
+++ lib/kamaze/project/core_ext/pp.rb
@@ -8,11 +8,12 @@
 
 require_relative '../../project'
 
-Kernel.tap do |c|
+Object.tap do |c|
   # @see https://ruby-doc.org/stdlib-2.4.0/libdoc/pp/rdoc/Kernel.html
   # @see https://github.com/topazproject/topaz/blob/bf4a56adbe03ae9ab4984729c733fcbc64a164c4/lib-ruby/pp.rb#L58
-  :pp.tap do |method|
-    c.define_method(method) do |*objs|
+  # @see https://github.com/ruby/ruby/blob/a8dc5156e183489c5121fb1759bda5d9406d9175/lib/pp.rb#L602
+  :pp.tap do |defined_method|
+    c.define_method(defined_method) do |*objs|
       Kamaze::Project::Debug.new.yield_self do |printer|
         objs.each { |obj| printer.dump(obj) }
 
@@ -20,6 +21,6 @@ Kernel.tap do |c|
       end
     end
 
-    [:private, :module_function].each { |v| c.__send__(v, method) }
+    [:private].each { |method_name| c.__send__(method_name, defined_method) }
   end
 end
