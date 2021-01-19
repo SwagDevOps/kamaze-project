62ccfe5b77ca2886a49536d799ae5dad76b6db8e
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-19 15:20:52 +0100

    inflector (helper) unclutter

--- lib/kamaze/project/helper/inflector.rb
+++ lib/kamaze/project/helper/inflector.rb
@@ -9,36 +9,18 @@
 require_relative '../helper'
 
 # Inflector built on top of ``Dry::Inflector``
-class Kamaze::Project::Helper::Inflector
-  def initialize
-    require 'dry/inflector'
-
-    @inflector = Dry::Inflector.new
-  end
-
+class Kamaze::Project::Helper::Inflector < Kamaze::Project::Inflector
   # Load constant from a loadable/requirable path
   #
   # @param [String] loadable
   # @return [Object]
+  #
+  # @raise LoadError
+  # @raise TypeError
   def resolve(loadable)
-    loadable = loadable.to_s.empty? ? nil : loadable.to_s
-
-    require loadable
-
-    @inflector.constantize(@inflector.classify(loadable))
-  end
-
-  def method_missing(method, *args, &block)
-    if respond_to_missing?(method)
-      @inflector.public_send(method, *args, &block)
-    else
-      super
+    (loadable.to_s.empty? ? nil : loadable.to_s).yield_self do |f|
+      require f
+      self.constantize(self.classify(f))
     end
-  end
-
-  def respond_to_missing?(method, include_private = false)
-    return true if @inflector.respond_to?(method, include_private)
-
-    super(method, include_private)
   end
 end
