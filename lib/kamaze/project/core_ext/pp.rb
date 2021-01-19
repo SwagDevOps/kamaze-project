94ac0cd4e3a4f1efd48d655aaf0f6de8fa07bd9a
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-19 18:31:17 +0100

    pp (core_ext) major rewrite

--- lib/kamaze/project/core_ext/pp.rb
+++ lib/kamaze/project/core_ext/pp.rb
@@ -6,23 +6,18 @@
 # This is free software: you are free to change and redistribute it.
 # There is NO WARRANTY, to the extent permitted by law.
 
-require_relative '../debug'
-
-# rubocop:disable Style/Documentation
-
-class Object
-  private
-
-  # Print arguments in pretty form
-  #
+Kernel.tap do |c|
   # @see https://ruby-doc.org/stdlib-2.4.0/libdoc/pp/rdoc/Kernel.html
-  # @see https://github.com/topazproject/topaz/blob/master/lib-ruby/pp.rb
-  def pp(*objs)
-    debug = Kamaze::Project::Debug.new
-    objs.each { |obj| debug.dump(obj) }
+  # @see https://github.com/topazproject/topaz/blob/bf4a56adbe03ae9ab4984729c733fcbc64a164c4/lib-ruby/pp.rb#L58
+  :pp.tap do |method|
+    c.define_method(method) do |*objs|
+      Kamaze::Project::Debug.new.yield_self do |printer|
+        objs.each { |obj| printer.dump(obj) }
 
-    objs.size <= 1 ? objs.first : objs
+        objs.size <= 1 ? objs.first : objs
+      end
+    end
+
+    [:private, :module_function].each { |v| c.__send__(v, method) }
   end
 end
-
-# rubocop:enable Style/Documentation
