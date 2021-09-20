276596597b720dff404288300e1f6da49a3f9836
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-09-20 19:29:28 +0200

    test (tasks) minor syntactic changes and improvements

--- lib/kamaze/project/tools/rspec.rb
+++ lib/kamaze/project/tools/rspec.rb
@@ -46,9 +46,10 @@ class Kamaze::Project::Tools::Rspec < Kamaze::Project::Tools::BaseTool
   # @return [self]
   def run
     with_exit_on_failure do
-      options = arguments.concat(options_arguments).map(&:to_s)
-
-      self.retcode = core.run(options, stderr, stdout).to_i
+      pp self
+      builder.call(arguments.concat(options_arguments)).yield_self do |runner|
+        self.retcode = runner.run(stderr, stdout).to_i
+      end
       reset
     end
 
@@ -97,10 +98,15 @@ class Kamaze::Project::Tools::Rspec < Kamaze::Project::Tools::BaseTool
   end
 
   # @return [RSpec::Core::Runner]
-  def core
+  def builder
     require 'rspec/core'
+    require 'rspec/core/configuration_options'
 
-    RSpec::Core::Runner
+    lambda do |options|
+      options = ::RSpec::Core::ConfigurationOptions.new(options.map(&:to_s))
+
+      ::Class.new(::RSpec::Core::Runner).new(options)
+    end
   end
 
   # @return [Array<String>]
